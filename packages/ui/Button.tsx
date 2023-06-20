0dc990f8f6d5d6dab907780af0dd5e9f2c6d1fe0
Shun Kakinoki <shunkakinoki@gmail.com>
2023-06-20 14:17:35 -0700

    chore: ini turbo ci

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -3,5 +3,9 @@
 import * as React from "react";
 
 export const Button = () => {
-  return <button onClick={() => alert("boop")}>Boop</button>;
+  return (
+    <button type="button" onClick={() => alert("boop")}>
+      Boop
+    </button>
+  );
 };
