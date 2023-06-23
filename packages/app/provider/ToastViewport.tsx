4cddd77850f298dd6b0257c3d8d9a8945374b549
Shun Kakinoki <shunkakinoki@gmail.com>
2023-06-22 20:12:50 -0700

    chore: update

--- packages/app/provider/ToastViewport.tsx
+++
@@ -1,7 +0,0 @@
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { ToastViewport as ToastViewportOg } from "ui";
-
-export const ToastViewport = () => {
-  const { top, right, left } = useSafeAreaInsets();
-  return <ToastViewportOg top={top + 5} left={left} right={right} />;
-};
--- packages/app/tamagui.config.ts
+++
@@ -1,9 +0,0 @@
-import { config } from "ui";
-
-export type Conf = typeof config;
-
-declare module "tamagui" {
-  interface TamaguiCustomConfig extends Conf {}
-}
-
-export default config;
