13145ab1866bf670b623851d40c330b91e6cd0a3
Shun Kakinoki <shunkakinoki@gmail.com>
2023-06-24 23:00:31 -0700

    chore: update

---
+++ .lintstagedrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "*.swift": ["pnpm run swift-format:cmd"],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "strict",
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "preserve",
+  requirePragma: false,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
