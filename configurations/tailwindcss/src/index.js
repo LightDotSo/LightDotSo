65d3081bad955c70a5efb37965ae2232fa3939de
Shun Kakinoki <shunkakinoki@gmail.com>
2023-06-27 18:32:36 -0700

    chore: ini tailwind

---
+++ configurations/tailwindcss/src/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: "class",
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+};
