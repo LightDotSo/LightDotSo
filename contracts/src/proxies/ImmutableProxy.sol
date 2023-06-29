b0c973a51aee57a2072dd7b42f40ade7875d57ba
Shun Kakinoki <shunkakinoki@gmail.com>
2023-06-29 13:51:44 -0700

    chore: update

--- contracts/src/proxies/ImmutableProxy.sol
+++ contracts/src/proxies/ImmutableProxy.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
 contract ImmutableProxy is UUPSUpgradeable {
-    function _authorizeUpgrade(address newImplementation) pure internal override {
+    function _authorizeUpgrade(address newImplementation) internal pure override {
         // Permanently disable upgrades by invalidating the authorization
         revert("Upgrades are disabled");
     }
