ab38a171ee86ab8f3b063e79148acaa717a2406f
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-06-29 13:21:16 -0700

    Update storage.sh

--- contracts/storage.sh
+++ contracts/storage.sh
@@ -5,11 +5,8 @@
 # Link: https://socket.dev/npm/package/@nocturne-xyz/contracts/files/1.0.32-alpha/storage_inspect.sh
 # License: MIT OR Apache-2.0
 
-# Define the contracts directory
-CONTRACTS_DIR=./contracts
-
 # Create a new file to hold all of the storage layouts
-OUTPUT_FILE=../.storage-layouts
+OUTPUT_FILE=.storage-layouts
 
 echo "" > "$OUTPUT_FILE"
 echo "============================" > "$OUTPUT_FILE"
