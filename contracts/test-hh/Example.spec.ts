47f1b7e037340ae4ff65a0650fa0345c792e2be5
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-02 12:45:13 -0700

    chore: update

---
+++ contracts/test-hh/Example.spec.ts
@@ -0,0 +1,8 @@
+import { ethers } from "hardhat";
+import { Signer } from "ethers";
+
+describe("Gnosis Proxy", function () {
+  this.timeout(30000);
+
+  it("should validate", async function () {});
+});
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-foundry");
+require("@nomiclabs/hardhat-ethers");
 
 const config = {
   solidity: "0.8.18",
