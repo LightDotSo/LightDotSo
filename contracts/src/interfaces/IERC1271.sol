672836e3a0db3c462799d837b14d140a9c51767e
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-07-02 14:54:54 -0700

    chore: ini userop test (#71)
    * chore: ini soul wallet
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: ini test hh
    
    * chore: update
    
    * chore: udpate
    
    * chore: update
    
    * chore: ini hardhat test
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: ini 4337 utils lib
    
    * chore: ini licensing
    
    * chore: refactor event to interface
    
    * chore: update

--- contracts/src/interfaces/IERC1271.sol
+++ contracts/src/interfaces/IERC1271.sol
@@ -7,5 +7,9 @@
 pragma solidity ^0.8.18;
 
 interface IERC1271 {
+    // -------------------------------------------------------------------------
+    // Actions
+    // -------------------------------------------------------------------------
+
     function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bytes4 magicValue);
 }
