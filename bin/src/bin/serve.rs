3d0f81117f82b5095dada6aaf2deb5d89fb4eb73
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-07-03 17:57:49 -0700

    chore: ini rust bin (#85)
    * chore: ini rust bin
    
    * chore: update
    
    * chore: update docker action
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update

---
+++ bin/src/bin/serve.rs
@@ -0,0 +1,17 @@
+use anyhow::Result;
+use axum::{routing::get, Router};
+
+pub async fn start_server() -> Result<()> {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let socket_addr = "0.0.0.0:3002".parse()?;
+    axum::Server::bind(&socket_addr).serve(app.into_make_service()).await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), anyhow::Error> {
+    start_server().await?;
+    Ok(())
+}
