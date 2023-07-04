57780868c546a7268e74ba2b5cad0d21d902e7c3
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-03 17:25:03 -0700

    chore: ini k8s health

--- bin/src/bin/serve.rs
+++ bin/src/bin/serve.rs
@@ -1,8 +1,14 @@
 use anyhow::Result;
 use axum::{routing::get, Router};
 
+async fn health_check() -> &'static str {
+    "OK"
+}
+
 pub async fn start_server() -> Result<()> {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/health", get(health_check));
 
     let socket_addr = "0.0.0.0:3002".parse()?;
     axum::Server::bind(&socket_addr).serve(app.into_make_service()).await?;
