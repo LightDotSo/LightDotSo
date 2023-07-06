e8e5ee7e1cefc117c34fff8b738f8705ce716716
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-05 19:42:30 -0700

    chore: ini script

--- .changeset/dull-forks-sell.md
+++
@@ -1,5 +0,0 @@
----
-"@lightdotso/bin": patch
----
-
-Add intial version
---
+++ contracts/script/LightWalletFactoryDeployer.s.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.18;
+
+import {LightWalletFactory} from "@/contracts/LightWalletFactory.sol";
+import "forge-std/Script.sol";
+
+interface ImmutableCreate2Factory {
+    function safeCreate2(bytes32 salt, bytes calldata initializationCode)
+        external
+        payable
+        returns (address deploymentAddress);
+}
+
+contract SeaportDeployer is Script {
+    address private constant EXPECTED_LIGHT_FACTORY_ADDRESS = address(0);
+    address private constant IMMUTABLE_CREATE2_FACTORY_ADDRESS = 0x0000000000FFe8B47B3e2130213B802212439497;
+    ImmutableCreate2Factory private constant IMMUTABLE_CREATE2_FACTORY =
+        ImmutableCreate2Factory(IMMUTABLE_CREATE2_FACTORY_ADDRESS);
+
+    function run() public {
+        // Use the private key to start a broadcast
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        // TODO: Specify salt for deterministic deployment
+        bytes32 salt = 0x0;
+
+        // Create LightWalletFactory
+        IMMUTABLE_CREATE2_FACTORY.safeCreate2(salt, type(LightWalletFactory).creationCode);
+        // address factory = IMMUTABLE_CREATE2_FACTORY.safeCreate2(salt, type(LightWalletFactory).creationCode);
+
+        // Assert that the factory is the expected address
+        // TODO: Add this assertion back in once we have a deterministic deployment
+        // assert(factory, EXPECTED_LIGHT_FACTORY_ADDRESS);
+
+        // Stop the broadcast
+        vm.stopBroadcast();
+    }
+}
