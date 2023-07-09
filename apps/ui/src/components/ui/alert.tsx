14ddb0753110b05c994e1c9c74c388fcd89aba7b
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-08 17:09:29 -0700

    chore: update

--- apps/ui/src/components/ui/alert.tsx
+++ apps/ui/src/components/ui/alert.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@lightdotso/ui/utils"
+import { cn } from "@lightdotso/utils"
 
 const alertVariants = cva(
   "relative w-full rounded-lg border p-4 [&:has(svg)]:pl-11 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
