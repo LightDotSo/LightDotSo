a4a977ec7a934ba7e64cbc1872f2efbd83d2d627
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-09 18:38:31 -0700

    chore: update license header

--- contracts/storage.sh
+++ contracts/storage.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+# Copyright (C) 2023 Light, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 # Code from: nocturne-xyz
 # Link: https://socket.dev/npm/package/@nocturne-xyz/contracts/files/1.0.32-alpha/storage_inspect.sh
