750064197b104810700b7be29110479e7d95b50a
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-10 21:36:53 -0700

    chore: update

---
+++ contracts/test/spec/LightWalletFactory.spec.ts
@@ -0,0 +1,30 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { describe, it, expect, test } from "vitest";
+//@ts-expect-error
+import { LightWalletFactory } from "@/contracts/LightWalletfactory.sol";
+
+test("LightWalletFactory: Correct humanReadableAbi", () => {
+  expect(Object.values(LightWalletFactory.humanReadableAbi))
+    .toMatchInlineSnapshot(`
+    [
+      "constructor(address _entryPoint)",
+      "function accountImplementation() view returns (address)",
+      "function createAccount(address owner, uint256 salt) returns (address ret)",
+      "function getAddress(address owner, uint256 salt) view returns (address)",
+    ]
+  `);
+});
