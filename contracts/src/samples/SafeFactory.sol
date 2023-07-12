e7118ec93b1e04ca516b9d6113e93dfdceb341d8
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-07-11 18:49:22 -0700

    chore: recover license (#160)
    * chore: refactor entrypoint constructor
    
    * chore: update
    
    * chore: ini eslint rc
    
    * chore: udpate
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: recover license

--- contracts/src/samples/SafeFactory.sol
+++ contracts/src/samples/SafeFactory.sol
@@ -20,7 +20,7 @@ pragma solidity ^0.8.18;
 // SafeL3.sol -- SafeL3 initial implementation
 // Modified implementation on SimpleAccountFactory.sol from @eth-infinitism/account-abstraction
 // Link: https://github.com/eth-infinitism/account-abstraction/blob/develop/contracts/samples/SimpleAccountFactory.sol
-// License: AGPL-3.0-or-later
+// License: GPL-3.0
 
 import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
