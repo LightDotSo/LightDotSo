ae0b61edad65a2297787ca6d495aefb8d50a5ee0
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-07-14 10:46:55 -0700

    chore: ini tailwind indicator (#169)
    * feat: ini shadcn ui
    
    * chore: update fix
    
    * chore: update
    
    * chore: update
    
    * chore: ini tailwind indicator

---
+++ apps/ui/src/components/dev/tailwind-indicator.tsx
@@ -0,0 +1,34 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"use client";
+
+const TailwindIndicator = () => {
+  if (process.env.NODE_ENV === "production") return null;
+
+  return (
+    <div className="fixed bottom-1 left-1 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
+      <div className="block sm:hidden">xs</div>
+      <div className="hidden sm:block md:hidden">sm</div>
+      <div className="hidden md:block lg:hidden">md</div>
+      <div className="hidden lg:block xl:hidden">lg</div>
+      <div className="hidden xl:block 2xl:hidden">xl</div>
+      <div className="hidden 2xl:block">2xl</div>
+    </div>
+  );
+};
+TailwindIndicator.displayName = "TailwindIndicator";
+
+export { TailwindIndicator };
