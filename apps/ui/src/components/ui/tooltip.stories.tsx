5e459e5d642699111db16e90a8d5c69081b06a52
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-14 10:22:45 -0700

    chore: update

--- apps/ui/src/components/ui/tooltip.stories.tsx
+++ apps/ui/src/components/ui/tooltip.stories.tsx
@@ -21,6 +21,7 @@ import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
+  TooltipProvider
 } from "@lightdotso/ui/components/ui/tooltip"
 
 const meta: Meta<typeof Tooltip> = {
@@ -35,17 +36,19 @@ type Story = StoryObj<typeof Tooltip>
 
 export const Base: Story = {
   render: (args) => (
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <Button variant="outline" className="w-10 rounded-full p-0">
-          <Plus className="h-4 w-4" />
-          <span className="sr-only">Add</span>
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent>
-        <p>Add to library</p>
-      </TooltipContent>
-    </Tooltip>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="outline" className="w-10 rounded-full p-0">
+            <Plus className="h-4 w-4" />
+            <span className="sr-only">Add</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Add to library</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   ),
   args: {},
 }
