315f79ce77487441c6544e4e0bf9db476c2ea4e4
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-15 15:18:21 -0700

    perf: update prisma-client-rust examples

---
+++ crates/indexer/src/error.rs
@@ -0,0 +1,38 @@
+/// Taken from official example: https://github.com/Brendonovich/prisma-client-rust/blob/124e8216a9d093e9ae1feb8b9b84614bc3579f18/examples/axum-rest/src/routes.rs
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+use prisma_client_rust::{
+    prisma_errors::query_engine::{RecordNotFound, UniqueKeyViolation},
+    QueryError,
+};
+
+pub enum AppError {
+    PrismaError(QueryError),
+    NotFound,
+}
+
+impl From<QueryError> for AppError {
+    fn from(error: QueryError) -> Self {
+        match error {
+            e if e.is_prisma_error::<RecordNotFound>() => AppError::NotFound,
+            e => AppError::PrismaError(e),
+        }
+    }
+}
+
+// This centralizes all different errors from our app in one place
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let status = match self {
+            AppError::PrismaError(error) if error.is_prisma_error::<UniqueKeyViolation>() => {
+                StatusCode::CONFLICT
+            }
+            AppError::PrismaError(_) => StatusCode::BAD_REQUEST,
+            AppError::NotFound => StatusCode::NOT_FOUND,
+        };
+
+        status.into_response()
+    }
+}
