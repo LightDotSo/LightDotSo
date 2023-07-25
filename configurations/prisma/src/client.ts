2de62beafbd0cff222304b87403d275fc7291744
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-24 22:47:02 -0700

    fix: prisma undefined

--- configurations/prisma/src/client.ts
+++ configurations/prisma/src/client.ts
@@ -19,10 +19,9 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma =
+const prisma =
   global.prisma ||
   new PrismaClient({ log: ["query", "info", "warn", "error"] });
-
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
-export * from "@prisma/client";
+export default prisma;
--- configurations/prisma/src/index.ts
+++ configurations/prisma/src/index.ts
@@ -13,6 +13,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-export * from "./client";
+export { default as prisma } from "./client";
 // eslint-disable-next-line import/no-unresolved
 export * from "./zod";
