662b496f91e67c036d1f7e64026b62096c1291a2
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-24 18:25:04 -0700

    chore: ini next ignore tracing output

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingIgnores: [
+      "node_modules/@swc/core-linux-x64-gnu",
+      "node_modules/@swc/core-linux-x64-musl",
+      "node_modules/esbuild-linux-64/bin",
+      "node_modules/webpack/lib",
+    ],
+  },
 };
 
 module.exports = nextConfig;
