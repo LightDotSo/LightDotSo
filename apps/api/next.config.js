7168583a58e3e522f7f0b108084def803c381de9
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-24 18:31:59 -0700

    chore: update

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -4,12 +4,12 @@ const nextConfig = {
   experimental: {
     outputFileTracingIgnores: {
       "*": [
-        "**/node_modules/@swc/core-linux-x64-gnu",
-        "**/node_modules/@swc/core-linux-x64-musl",
-        "**/node_modules/esbuild-linux-64/bin",
-        "**/node_modules/webpack/lib",
-        "**/node_modules/rollup",
-        "**/node_modules/terser",
+        "./../../node_modules/@swc/core-linux-x64-gnu",
+        "./../../node_modules/@swc/core-linux-x64-musl",
+        "./../../node_modules/esbuild-linux-64/bin",
+        "./../../node_modules/webpack/lib",
+        "./../../node_modules/rollup",
+        "./../../node_modules/terser",
       ],
     },
   },
