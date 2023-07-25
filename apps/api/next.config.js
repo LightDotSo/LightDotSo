3258afaeb4de285875554fb549932aeaabc39002
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-24 18:37:28 -0700

    chore: update

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -2,16 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    outputFileTracingIgnores: {
-      "*": [
-        "./../../node_modules/@swc/core-linux-x64-gnu",
-        "./../../node_modules/@swc/core-linux-x64-musl",
-        "./../../node_modules/esbuild-linux-64/bin",
-        "./../../node_modules/webpack/lib",
-        "./../../node_modules/rollup",
-        "./../../node_modules/terser",
-      ],
-    },
+    // From: https://github.com/vercel/next.js/issues/42641
+    outputFileTracingIgnores: [
+      "./node_modules/@swc/core-linux-x64-gnu",
+      "./node_modules/@swc/core-linux-x64-musl",
+      "./node_modules/esbuild-linux-64/bin",
+      "./node_modules/webpack/lib",
+      "./node_modules/rollup",
+      "./node_modules/terser",
+    ],
   },
   outputFileTracing: true,
 };
