6a815e03bfa478ae7d6bdb68bae5c0a071ce486f
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-24 18:51:42 -0700

    chore: update

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -3,20 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     // From: https://github.com/vercel/next.js/issues/42641
-    outputFileTracingIgnores: [
-      "./../../node_modules/@swc/core-linux-x64-gnu",
-      "./../../node_modules/@swc/core-linux-x64-musl",
-      "./../../node_modules/esbuild-linux-64/bin",
-      "./../../node_modules/webpack/lib",
-      "./../../node_modules/rollup",
-      "./../../node_modules/terser",
-      "./node_modules/@swc/core-linux-x64-gnu",
-      "./node_modules/@swc/core-linux-x64-musl",
-      "./node_modules/esbuild-linux-64/bin",
-      "./node_modules/webpack/lib",
-      "./node_modules/rollup",
-      "./node_modules/terser",
-    ],
+    outputFileTracingExcludes: {
+      "*": [
+        "./../../node_modules/@swc/core-linux-x64-gnu",
+        "./../../node_modules/@swc/core-linux-x64-musl",
+        "./../../node_modules/esbuild-linux-64/bin",
+        "./../../node_modules/webpack/lib",
+        "./../../node_modules/rollup",
+        "./../../node_modules/terser",
+        "./node_modules/@swc/core-linux-x64-gnu",
+        "./node_modules/@swc/core-linux-x64-musl",
+        "./node_modules/esbuild-linux-64/bin",
+        "./node_modules/webpack/lib",
+        "./node_modules/rollup",
+        "./node_modules/terser",
+      ],
+    },
   },
   outputFileTracing: true,
 };
