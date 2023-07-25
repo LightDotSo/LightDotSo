f915aaa186c22b2bcccb8e1cf6f445e973293e78
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-24 18:45:31 -0700

    chore: update

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   experimental: {
     // From: https://github.com/vercel/next.js/issues/42641
     outputFileTracingIgnores: [
+      "./../../node_modules/@swc/core-linux-x64-gnu",
+      "./../../node_modules/@swc/core-linux-x64-musl",
+      "./../../node_modules/esbuild-linux-64/bin",
+      "./../../node_modules/webpack/lib",
+      "./../../node_modules/rollup",
+      "./../../node_modules/terser",
       "./node_modules/@swc/core-linux-x64-gnu",
       "./node_modules/@swc/core-linux-x64-musl",
       "./node_modules/esbuild-linux-64/bin",
