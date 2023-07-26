43d86cd250c622884fbcad63c95ac0cfa4db2439
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-07-26 11:40:28 -0700

    feat: ini checkly testing (#275)
    * feat: ini checkly testing
    
    * chore: update
    
    * chore: update
    
    * chore: ini app check

---
+++ apps/app/__checks__/healthcheck.check.ts
@@ -0,0 +1,61 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { ApiCheck, AssertionBuilder, CheckGroup } from "checkly/constructs";
+
+export const websiteGroup = new CheckGroup("app", {
+  name: "App Group",
+  activated: true,
+  muted: false,
+  runtimeId: "2022.10",
+  locations: ["us-east-1", "eu-west-1", "ap-northeast-1"],
+  tags: ["app", "group"],
+  environmentVariables: [],
+  apiCheckDefaults: {},
+  concurrency: 100,
+  alertChannels: [],
+});
+
+const targetUrl = process.env.ENVIRONMENT_URL || "https://api.light.so";
+
+new ApiCheck("app-healthcheck", {
+  name: "App Healthcheck",
+  group: websiteGroup,
+  activated: true,
+  muted: false,
+  doubleCheck: true,
+  shouldFail: false,
+  locations: ["eu-west-1", "us-west-1"],
+  tags: ["api"],
+  frequency: 60,
+  environmentVariables: [],
+  maxResponseTime: 20000,
+  degradedResponseTime: 50000,
+  request: {
+    url: `${targetUrl}/api/trpc/healthcheck`,
+    method: "GET",
+    followRedirects: true,
+    skipSSL: false,
+    assertions: [
+      AssertionBuilder.jsonBody("").hasKey("result"),
+      AssertionBuilder.statusCode().equals(200),
+      AssertionBuilder.responseTme().lessThan(4500),
+    ],
+    body: "",
+    bodyType: "JSON",
+    headers: [],
+    queryParameters: [],
+  },
+});
