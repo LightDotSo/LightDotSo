beb2184cc97b9acd91f4b80150c9549adbfaf735
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-26 18:18:00 -0700

    feat: ini auth

--- apps/app/pages/api/auth/[...nextauth].ts
+++ apps/app/pages/api/auth/[...nextauth].ts
@@ -13,81 +13,23 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { getCsrfToken } from "next-auth/react";
-import { SiweMessage } from "siwe";
-import { prisma } from "@lightdotso/prisma";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import type { NextApiRequest, NextApiResponse } from "next";
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import { authOptions } from "@lightdotso/auth";
 
-// From: https://github.com/spruceid/siwe-next-auth-example
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
-export default async function auth(req: any, res: any) {
-  const providers = [
-    CredentialsProvider({
-      name: "Ethereum",
-      credentials: {
-        message: {
-          label: "Message",
-          type: "text",
-          placeholder: "0x0",
-        },
-        signature: {
-          label: "Signature",
-          type: "text",
-          placeholder: "0x0",
-        },
-      },
-      async authorize(credentials) {
-        try {
-          const siwe = new SiweMessage(
-            JSON.parse(credentials?.message || "{}"),
-          );
-          const nextAuthUrl = new URL(process.env.NEXTAUTH_URL!);
-
-          const result = await siwe.verify({
-            signature: credentials?.signature || "",
-            domain: nextAuthUrl.host,
-            nonce: await getCsrfToken({ req: { headers: req.headers } }),
-          });
-
-          if (result.success) {
-            return {
-              id: siwe.address,
-            };
-          }
-          return null;
-        } catch (e) {
-          return null;
-        }
-      },
-    }),
-  ];
+const Auth = (req: NextApiRequest, res: NextApiResponse) => {
+  const authOpts: NextAuthOptions = authOptions({ req });
 
   const isDefaultSigninPage =
-    req.method === "GET" && req.query.nextauth.includes("signin");
+    req.method === "GET" && req?.query?.nextauth?.includes("signin");
 
   // Hide Sign-In with Ethereum from default sign page
   if (isDefaultSigninPage) {
-    providers.pop();
+    // Removes from the authOptions.providers array
+    authOpts.providers.pop();
   }
 
-  return await NextAuth(req, res, {
-    // https://next-auth.js.org/configuration/providers/oauth
-    providers,
-    session: {
-      strategy: "jwt",
-    },
-    adapter: PrismaAdapter(prisma),
-    secret: process.env.NEXTAUTH_SECRET!,
-    callbacks: {
-      async session({ session, token }: { session: any; token: any }) {
-        session.address = token.sub;
-        session.user.name = token.sub;
-        session.user.image = "/";
-        return session;
-      },
-    },
-  });
-}
+  return NextAuth(req, res, authOpts) as typeof NextAuth;
+};
+
+export default Auth;
---
+++ packages/auth/index.ts
@@ -0,0 +1,91 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import type { CtxOrReq } from "next-auth/client/_utils";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { getCsrfToken } from "next-auth/react";
+import { SiweMessage } from "siwe";
+import { prisma } from "@lightdotso/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { getServerSession } from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import { type GetServerSidePropsContext } from "next";
+
+export const authOptions: (ctxReq: CtxOrReq) => NextAuthOptions = ({
+  req,
+}) => ({
+  providers: [
+    CredentialsProvider({
+      name: "Ethereum",
+      credentials: {
+        message: {
+          label: "Message",
+          type: "text",
+          placeholder: "0x0",
+        },
+        signature: {
+          label: "Signature",
+          type: "text",
+          placeholder: "0x0",
+        },
+      },
+      async authorize(credentials) {
+        try {
+          const siwe = new SiweMessage(
+            JSON.parse(credentials?.message || "{}"),
+          );
+          const nextAuthUrl = new URL(process.env.NEXTAUTH_URL!);
+
+          const result = await siwe.verify({
+            signature: credentials?.signature || "",
+            domain: nextAuthUrl.host,
+            nonce: await getCsrfToken({ req: { headers: req?.headers } }),
+          });
+
+          if (result.success) {
+            return {
+              id: siwe.address,
+            };
+          }
+          return null;
+        } catch (e) {
+          return null;
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET!,
+  callbacks: {
+    async session({ session, token }: { session: any; token: any }) {
+      session.address = token.sub;
+      session.user.name = token.sub;
+      session.user.image = "/";
+      return session;
+    },
+  },
+});
+
+export const getServerAuthSession = async (ctx: {
+  req: GetServerSidePropsContext["req"];
+  res: GetServerSidePropsContext["res"];
+}) => {
+  // Changed from authOptions to authOption(ctx)
+  // This allows use to retrieve the csrf token to verify as the nonce
+  return getServerSession(ctx.req, ctx.res, authOptions(ctx));
+};
