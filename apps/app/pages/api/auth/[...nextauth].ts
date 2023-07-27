cfddce5012443b208bf51bec42084acf1a9d2aff
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-07-26 17:25:30 -0700

    feat: ini next auth (#279)

---
+++ apps/app/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,93 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { getCsrfToken } from "next-auth/react";
+import { SiweMessage } from "siwe";
+import { prisma } from "@lightdotso/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+// From: https://github.com/spruceid/siwe-next-auth-example
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export default async function auth(req: any, res: any) {
+  const providers = [
+    CredentialsProvider({
+      name: "Ethereum",
+      credentials: {
+        message: {
+          label: "Message",
+          type: "text",
+          placeholder: "0x0",
+        },
+        signature: {
+          label: "Signature",
+          type: "text",
+          placeholder: "0x0",
+        },
+      },
+      async authorize(credentials) {
+        try {
+          const siwe = new SiweMessage(
+            JSON.parse(credentials?.message || "{}"),
+          );
+          const nextAuthUrl = new URL(process.env.NEXTAUTH_URL!);
+
+          const result = await siwe.verify({
+            signature: credentials?.signature || "",
+            domain: nextAuthUrl.host,
+            nonce: await getCsrfToken({ req: { headers: req.headers } }),
+          });
+
+          if (result.success) {
+            return {
+              id: siwe.address,
+            };
+          }
+          return null;
+        } catch (e) {
+          return null;
+        }
+      },
+    }),
+  ];
+
+  const isDefaultSigninPage =
+    req.method === "GET" && req.query.nextauth.includes("signin");
+
+  // Hide Sign-In with Ethereum from default sign page
+  if (isDefaultSigninPage) {
+    providers.pop();
+  }
+
+  return await NextAuth(req, res, {
+    // https://next-auth.js.org/configuration/providers/oauth
+    providers,
+    session: {
+      strategy: "jwt",
+    },
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.NEXTAUTH_SECRET!,
+    callbacks: {
+      async session({ session, token }: { session: any; token: any }) {
+        session.address = token.sub;
+        session.user.name = token.sub;
+        session.user.image = "/";
+        return session;
+      },
+    },
+  });
+}
