384ea7ce4e6569335abd03331a39e37790464a6f
Shun Kakinoki <shunkakinoki@gmail.com>
2023-07-29 00:49:47 -0700

    perf: ini fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,12 +5,40 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 const baseUrl = process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3001";
 console.log(`Using base URL "${baseUrl}"`);
 
+// From: https://github.com/calcom/cal.com/blob/50d2dad62c5cc93b9ccfad6b9b0836062fd5f465/playwright.config.ts#L26
+const IS_API_TEST = process.argv.some(a =>
+  a.startsWith("--project=@lightdotso/api"),
+);
+
+const webServer: PlaywrightTestConfig["webServer"] = [
+  {
+    command: "pnpm turbo run dev --filter @lightdotso/app",
+    port: 3001,
+    timeout: 60_000,
+    reuseExistingServer: !process.env.CI,
+  },
+];
+
+if (IS_API_TEST) {
+  webServer.push({
+    command: "pnpm turbo run dev --filter @lightdotso/api",
+    port: 3000,
+    timeout: 60_000,
+    reuseExistingServer: !process.env.CI,
+  });
+}
+
 const opts = {
   headless: !!process.env.CI || !!process.env.PLAYWRIGHT_HEADLESS,
   collectCoverage: !!process.env.PLAYWRIGHT_HEADLESS,
 };
 const config: PlaywrightTestConfig = {
   projects: [
+    {
+      name: "@lightdotso/api",
+      testDir: "./apps/api/e2e",
+      testMatch: /.*\.spec\.tsx?/,
+    },
     {
       name: "@lightdotso/app",
       testDir: "./apps/app/e2e",
@@ -21,6 +49,7 @@ const config: PlaywrightTestConfig = {
     baseURL: baseUrl,
     headless: opts.headless,
   },
+  webServer,
 };
 
 export default config;
