cf0dd2d46130578db716b618b44f86d91c236cec
Shun Kakinoki <shunkakinoki@gmail.com>
2023-08-08 00:06:30 -0700

    feat: ini consumer

---
+++ bin/src/bin/consumer.rs
@@ -0,0 +1,71 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use std::sync::Arc;
+
+use anyhow::Result;
+use axum::{routing::get, Router};
+use clap::Parser;
+use dotenvy::dotenv;
+use lightdotso_bin::version::SHORT_VERSION;
+use lightdotso_db::db::create_client;
+use lightdotso_indexer::config::IndexerArgs;
+use lightdotso_tracing::{
+    init, stdout,
+    tracing::{info, Level},
+};
+
+async fn health_check() -> &'static str {
+    "OK"
+}
+
+pub async fn start_server() -> Result<()> {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/health", get(health_check));
+
+    let socket_addr = "0.0.0.0:3002".parse()?;
+    axum::Server::bind(&socket_addr).serve(app.into_make_service()).await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+pub async fn main() {
+    let _ = dotenv();
+
+    init(vec![stdout(Level::INFO)]);
+
+    info!("Starting server at {}", SHORT_VERSION);
+
+    // Parse the command line arguments
+    let args = IndexerArgs::parse();
+
+    // Create the db client
+    let db = Arc::new(create_client().await.unwrap());
+
+    // Construct the futures
+    let indexer_future = args.run(db);
+    let server_future = start_server();
+
+    // Run the futures concurrently
+    let result = tokio::try_join!(indexer_future, server_future);
+
+    // Exit with an error if either future failed
+    if let Err(e) = result {
+        eprintln!("Error: {:?}", e);
+        std::process::exit(1);
+    }
+}
---
+++ crates/consumer/src/config.rs
@@ -0,0 +1,92 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use crate::consumer::Consumer;
+use anyhow::Result;
+use clap::Parser;
+use tracing::info;
+
+#[derive(Debug, Clone, Parser, Default)]
+pub struct ConsumerArgs {
+    /// The group id of the consumer.
+    #[arg(long, short, default_value_t = String::from(""))]
+    #[clap(long, env = "KAFKA_GROUP")]
+    pub group: String,
+    /// The topics to consume.
+    #[arg(long, short, num_args = 1.., value_delimiter = ',')]
+    #[clap(long, env = "KAFKA_TOPICS")]
+    pub topics: Vec<String>,
+}
+
+impl ConsumerArgs {
+    pub async fn run(&self) -> Result<()> {
+        // Add info
+        info!("ConsumerArgs run, exiting");
+
+        // Print the config
+        info!("Config: {:?}", self);
+
+        let consumer = Consumer::new(self).await;
+
+        consumer.run().await;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    #[test]
+    fn test_config_values() {
+        // Reset the env vars
+        env::remove_var("KAFKA_GROUP");
+        env::remove_var("KAFKA_TOPICS");
+
+        // Create a Config with default values
+        let config_args = ConsumerArgs::parse_from([""]);
+
+        // Verify the default values
+        assert_eq!(config_args.group, "");
+        assert_eq!(config_args.topics, vec![""]);
+
+        // Set some env vars
+        env::set_var("KAFKA_GROUP", "one");
+        env::set_var("KAFKA_TOPICS", "hello,world");
+
+        // Create a Config with env values
+        let config_args = ConsumerArgs::parse_from([""]);
+
+        // Verify the new values from env
+        assert_eq!(config_args.group, "one");
+        assert_eq!(config_args.topics, vec!["hello", "world"]);
+
+        // Reset the env vars
+        env::remove_var("KAFKA_GROUP");
+        env::remove_var("KAFKA_TOPICS");
+    }
+
+    #[test]
+    fn test_config_default_values() {
+        // Create a Config with default values
+        let config_args = ConsumerArgs::default();
+
+        // Verify the default values
+        assert_eq!(config_args.group, "");
+        assert_eq!(config_args.topics, vec![""]);
+    }
+}
