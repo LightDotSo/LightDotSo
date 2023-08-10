5fbb88031d1b4ebf8d4fcbae9af4a872a5063f04
Shun Kakinoki <shunkakinoki@gmail.com>
2023-08-09 20:56:19 -0700

    fix: build explorer

---
+++ apps/explorer/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,30 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { createContext, appRouter } from "@lightdotso/trpc";
+
+// Add back once NextAuth v5 is released
+// export const runtime = 'edge';
+
+const handler = (req: Request) =>
+  fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext,
+  });
+
+export { handler as GET, handler as POST };
