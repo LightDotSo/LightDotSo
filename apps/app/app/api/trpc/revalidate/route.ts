47740ee181502f6de1ccd398c1fbab9c299b66d0
Shun Kakinoki <shunkakinoki@gmail.com>
2023-08-10 16:28:33 -0700

    fix: nextjs headers

--- apps/app/app/api/trpc/revalidate/route.ts
+++ apps/app/app/api/trpc/revalidate/route.ts
@@ -13,21 +13,16 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-export { experimental_revalidateEndpoint } from "@trpc/next/app-dir/server";
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-// Add back once NextAuth v5 is released
-// export const runtime = 'edge';
-
-const handler = async (req: Request) => {
-  const res = await experimental_revalidateEndpoint(req);
-  return new Response(res.body, {
-    status: res.status,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
-  });
-};
-
-export { handler as POST };
+export { experimental_revalidateEndpoint as POST } from "@trpc/next/app-dir/server";
