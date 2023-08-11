e7f5093b2e784ed69d0853a8e161b9fb1efce412
Shun Kakinoki <shunkakinoki@gmail.com>
2023-08-10 17:53:07 -0700

    chore: ini cors

--- apps/playground/next.config.js
+++ apps/playground/next.config.js
@@ -33,7 +33,7 @@ const nextConfig = {
           {
             key: "Access-Control-Allow-Headers",
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x-vercel-ip-latitude",
           },
         ],
       },
