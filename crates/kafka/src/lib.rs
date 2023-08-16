8f7ad878ea274851e26dbc690e51ddf77ad335a4
Shun Kakinoki <shunkakinoki@gmail.com>
2023-08-15 18:24:48 -0700

    perf: ini kafka local

--- crates/kafka/src/lib.rs
+++ crates/kafka/src/lib.rs
@@ -13,6 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use lightdotso_tracing::tracing::error;
 pub use rdkafka;
 
 use rdkafka::{
@@ -21,14 +22,31 @@ use rdkafka::{
 
 /// Configure a Kafka client with the required settings.
 pub fn configure_client(group: &str) -> Result<ClientConfig, Box<dyn std::error::Error>> {
+    // Get the environment variables
+    let broker = std::env::var("KAFKA_BROKER")?;
+    let username = std::env::var("KAFKA_USERNAME").unwrap_or("".to_string());
+    let password = std::env::var("KAFKA_PASSWORD").unwrap_or("".to_string());
+
     let mut binding = ClientConfig::new();
+
+    // If host is localhost, connect to kafka without security settings.
+    if broker.starts_with("localhost") {
+        let config = binding
+            .set("group.id", group)
+            .set("bootstrap.servers", broker)
+            .set("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+            .set("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        return Ok(config.clone());
+    }
+
     let config = binding
         .set("group.id", group)
-        .set("bootstrap.servers", std::env::var("KAFKA_BROKER")?)
+        .set("bootstrap.servers", broker)
         .set("sasl.mechanism", "SCRAM-SHA-256")
         .set("security.protocol", "SASL_SSL")
-        .set("sasl.username", std::env::var("KAFKA_USERNAME")?)
-        .set("sasl.password", std::env::var("KAFKA_PASSWORD")?);
+        .set("sasl.username", username)
+        .set("sasl.password", password);
 
     Ok(config.clone())
 }
@@ -47,6 +65,7 @@ pub fn get_producer() -> Result<BaseProducer, rdkafka::error::KafkaError> {
     // Ignores the group id for producers.
     let client_config = configure_client("");
     if client_config.is_err() {
+        error!("Failed to create client");
         return Err(rdkafka::error::KafkaError::ClientCreation("Failed to create client".into()));
     }
     client_config.unwrap().create()
