387e57d57543901f9e102c4cec397d071206bf8e
Shun Kakinoki <shunkakinoki@gmail.com>
2023-08-27 23:24:16 -0700

    chore: update

---
+++ apps/api/app/swagger/page.tsx
@@ -0,0 +1,28 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"use client";
+
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+
+// Url for local development, vercel deployment
+const url = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc/openapi.json`
+  : "http://localhost:3000/api/trpc/openapi.json";
+
+export default function Page() {
+  return <SwaggerUI url={url} />;
+}
