5b30d07631a4e24723e2d1bcfeadc12bc5e51639
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-09-01 06:31:26 -0700

    fix: kafka consumer (#481)
    * fix: kafka consumer
    
    * chore: update

--- crates/kafka/src/lib.rs
+++ crates/kafka/src/lib.rs
@@ -59,10 +59,9 @@ pub fn configure_client(group: &str) -> Result<ClientConfig, Box<dyn std::error:
 }
 
 /// Get a Kafka consumer with the required settings.
-pub fn get_consumer() -> Result<StreamConsumer, rdkafka::error::KafkaError> {
-    // Set the group to process env `FLY_ALLOC_ID` if it exists, otherwise use default.
-    let client_config =
-        configure_client(std::env::var("FLY_ALLOC_ID").unwrap_or("default".to_string()).as_str());
+pub fn get_consumer(group: &str) -> Result<StreamConsumer, rdkafka::error::KafkaError> {
+    // Set the group to the specified group.
+    let client_config = configure_client(group);
     if client_config.is_err() {
         return Err(rdkafka::error::KafkaError::ClientCreation("Failed to create client".into()));
     }
