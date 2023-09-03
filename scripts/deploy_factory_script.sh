6933355cdcc4029ed4163798479406a0689a8dbf
Shun Kakinoki <shunkakinoki@gmail.com>
2023-09-03 01:15:00 -0700

    chore: deploy and update scripts

--- scripts/deploy_factory_script.sh
+++ scripts/deploy_factory_script.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 echo "Deploying factory script..."
 echo $DEPLOY_RPC_URL
 
-forge script contracts/script/LightWalletFactoryDeployer.s.sol \
+FOUNDRY_PROFILE="deploy" forge script contracts/script/LightWalletFactoryDeployer.s.sol \
     --tc LightWalletFactoryDeployer \
     -f $DEPLOY_RPC_URL \
     --sender 0x35da762a35FCb3160738EeCd60fa18438C273D5E \
