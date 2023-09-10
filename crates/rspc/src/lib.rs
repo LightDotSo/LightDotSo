458cf3a867cc47873fb2c484123524cbc76cd28e
Shun Kakinoki <shunkakinoki@gmail.com>
2023-09-10 06:06:13 -0700

    feat: ini rspc

--- crates/rspc/src/lib.rs
+++ crates/rspc/src/lib.rs
@@ -13,6 +13,54 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pub fn rust_greeting(to: String) -> String {
-    format!("Hello World, {}!", to)
+use lightdotso_prisma::PrismaClient;
+use lightdotso_tracing::tracing::info;
+use rspc::{Config, Router, Type};
+use serde::{Deserialize, Serialize};
+use std::{path::PathBuf, sync::Arc, thread::sleep, time::Duration};
+
+#[derive(Debug, Clone)]
+pub struct Context {
+    pub db: Arc<PrismaClient>,
+}
+
+pub fn create_router() -> Router<Context> {
+    Router::<Context>::new()
+        .config(Config::new().set_ts_bindings_header("/* eslint-disable */").export_ts_bindings(
+            PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../src/types/bindings.ts"),
+        ))
+        .query("version", |t| t(|_, _: ()| env!("CARGO_PKG_VERSION")))
+        .query("wallets", |t| {
+            t(|ctx, _: ()| async move {
+                let wallets = ctx.db.wallet().find_many(vec![]).exec().await?;
+
+                Ok(wallets)
+            })
+        })
+        // Subscriptions can also be used for server -> client real time events
+        // Subscriptions have a slightly different syntax. You can respond with any Rust `Stream`
+        // type.
+        .subscription("pings", |t| {
+            t(|_, _: ()| {
+                async_stream::stream! {
+                    for _i in 0..5 {
+                        yield "ping".to_string();
+                        sleep(Duration::from_secs(1));
+                    }
+                }
+            })
+        })
+        .mutation("emptyWallets", |t| {
+            #[derive(Debug, Clone, Deserialize, Serialize, Type)]
+            struct EmptyInput {
+                content: String,
+            }
+            t(|ctx, empty: EmptyInput| async move {
+                info!("createPost: {:?}", empty);
+                let wallets = ctx.db.wallet().find_many(vec![]).exec().await?;
+
+                Ok(wallets)
+            })
+        })
+        .build()
 }
