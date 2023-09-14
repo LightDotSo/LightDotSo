dccf988d5fa8f6f36f4612308a1266127fd9745a
Shun Kakinoki <shunkakinoki@gmail.com>
2023-09-14 16:15:59 -0700

    perf: ini event handlers

--- apps/subgraph/src/entry-point.ts
+++ apps/subgraph/src/entry-point.ts
@@ -15,168 +15,146 @@
 
 import {
   AccountDeployed as AccountDeployedEvent,
-  BeforeExecution as BeforeExecutionEvent,
-  Deposited as DepositedEvent,
-  SignatureAggregatorChanged as SignatureAggregatorChangedEvent,
-  StakeLocked as StakeLockedEvent,
-  StakeUnlocked as StakeUnlockedEvent,
-  StakeWithdrawn as StakeWithdrawnEvent,
+  // BeforeExecution as BeforeExecutionEvent,
+  // Deposited as DepositedEvent,
+  // SignatureAggregatorChanged as SignatureAggregatorChangedEvent,
+  // StakeLocked as StakeLockedEvent,
+  // StakeUnlocked as StakeUnlockedEvent,
+  // StakeWithdrawn as StakeWithdrawnEvent,
   UserOperationEvent as UserOperationEventEvent,
   UserOperationRevertReason as UserOperationRevertReasonEvent,
-  Withdrawn as WithdrawnEvent,
+  // Withdrawn as WithdrawnEvent,
 } from "../generated/EntryPoint/EntryPoint";
+import // AccountDeployed,
+// BeforeExecution,
+// Deposited,
+// SignatureAggregatorChanged,
+// StakeLocked,
+// StakeUnlocked,
+// StakeWithdrawn,
+// UserOperationEvent,
+// UserOperationRevertReason,
+// Withdrawn,
+"../generated/schema";
 import {
-  // AccountDeployed,
-  BeforeExecution,
-  Deposited,
-  SignatureAggregatorChanged,
-  StakeLocked,
-  StakeUnlocked,
-  StakeWithdrawn,
-  UserOperationEvent,
-  UserOperationRevertReason,
-  Withdrawn,
-} from "../generated/schema";
-import { handleLightWalletDeployed } from "./light-wallet";
+  handleLightWalletDeployed,
+  handleLightWalletUserOperationEvent,
+  handleLightWalletUserOperationRevertReason,
+} from "./light-wallet";
 
 export function handleAccountDeployed(event: AccountDeployedEvent): void {
   // Handle if the account is LightWallet
   handleLightWalletDeployed(event);
 }
 
-export function handleBeforeExecution(event: BeforeExecutionEvent): void {
-  let entity = new BeforeExecution(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  );
+// export function handleBeforeExecution(event: BeforeExecutionEvent): void {
+//   let entity = new BeforeExecution(
+//     event.transaction.hash.concatI32(event.logIndex.toI32()),
+//   );
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+//   entity.blockNumber = event.block.number;
+//   entity.blockTimestamp = event.block.timestamp;
+//   entity.transactionHash = event.transaction.hash;
 
-  entity.save();
-}
+//   entity.save();
+// }
 
-export function handleDeposited(event: DepositedEvent): void {
-  let entity = new Deposited(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  );
-  entity.account = event.params.account;
-  entity.totalDeposit = event.params.totalDeposit;
+// export function handleDeposited(event: DepositedEvent): void {
+//   let entity = new Deposited(
+//     event.transaction.hash.concatI32(event.logIndex.toI32()),
+//   );
+//   entity.account = event.params.account;
+//   entity.totalDeposit = event.params.totalDeposit;
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+//   entity.blockNumber = event.block.number;
+//   entity.blockTimestamp = event.block.timestamp;
+//   entity.transactionHash = event.transaction.hash;
 
-  entity.save();
-}
+//   entity.save();
+// }
 
-export function handleSignatureAggregatorChanged(
-  event: SignatureAggregatorChangedEvent,
-): void {
-  let entity = new SignatureAggregatorChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  );
-  entity.aggregator = event.params.aggregator;
+// export function handleSignatureAggregatorChanged(
+//   event: SignatureAggregatorChangedEvent,
+// ): void {
+//   let entity = new SignatureAggregatorChanged(
+//     event.transaction.hash.concatI32(event.logIndex.toI32()),
+//   );
+//   entity.aggregator = event.params.aggregator;
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+//   entity.blockNumber = event.block.number;
+//   entity.blockTimestamp = event.block.timestamp;
+//   entity.transactionHash = event.transaction.hash;
 
-  entity.save();
-}
+//   entity.save();
+// }
 
-export function handleStakeLocked(event: StakeLockedEvent): void {
-  let entity = new StakeLocked(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  );
-  entity.account = event.params.account;
-  entity.totalStaked = event.params.totalStaked;
-  entity.unstakeDelaySec = event.params.unstakeDelaySec;
+// export function handleStakeLocked(event: StakeLockedEvent): void {
+//   let entity = new StakeLocked(
+//     event.transaction.hash.concatI32(event.logIndex.toI32()),
+//   );
+//   entity.account = event.params.account;
+//   entity.totalStaked = event.params.totalStaked;
+//   entity.unstakeDelaySec = event.params.unstakeDelaySec;
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+//   entity.blockNumber = event.block.number;
+//   entity.blockTimestamp = event.block.timestamp;
+//   entity.transactionHash = event.transaction.hash;
 
-  entity.save();
-}
+//   entity.save();
+// }
 
-export function handleStakeUnlocked(event: StakeUnlockedEvent): void {
-  let entity = new StakeUnlocked(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  );
-  entity.account = event.params.account;
-  entity.withdrawTime = event.params.withdrawTime;
+// export function handleStakeUnlocked(event: StakeUnlockedEvent): void {
+//   let entity = new StakeUnlocked(
+//     event.transaction.hash.concatI32(event.logIndex.toI32()),
+//   );
+//   entity.account = event.params.account;
+//   entity.withdrawTime = event.params.withdrawTime;
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+//   entity.blockNumber = event.block.number;
+//   entity.blockTimestamp = event.block.timestamp;
+//   entity.transactionHash = event.transaction.hash;
 
-  entity.save();
-}
+//   entity.save();
+// }
 
-export function handleStakeWithdrawn(event: StakeWithdrawnEvent): void {
-  let entity = new StakeWithdrawn(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  );
-  entity.account = event.params.account;
-  entity.withdrawAddress = event.params.withdrawAddress;
-  entity.amount = event.params.amount;
+// export function handleStakeWithdrawn(event: StakeWithdrawnEvent): void {
+//   let entity = new StakeWithdrawn(
+//     event.transaction.hash.concatI32(event.logIndex.toI32()),
+//   );
+//   entity.account = event.params.account;
+//   entity.withdrawAddress = event.params.withdrawAddress;
+//   entity.amount = event.params.amount;
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+//   entity.blockNumber = event.block.number;
+//   entity.blockTimestamp = event.block.timestamp;
+//   entity.transactionHash = event.transaction.hash;
 
-  entity.save();
-}
+//   entity.save();
+// }
 
 export function handleUserOperationEvent(event: UserOperationEventEvent): void {
-  let entity = new UserOperationEvent(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  );
-  entity.userOpHash = event.params.userOpHash;
-  entity.sender = event.params.sender;
-  entity.paymaster = event.params.paymaster;
-  entity.nonce = event.params.nonce;
-  entity.success = event.params.success;
-  entity.actualGasCost = event.params.actualGasCost;
-  entity.actualGasUsed = event.params.actualGasUsed;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
+  // Handle if the account is LightWallet
+  handleLightWalletUserOperationEvent(event);
 }
 
 export function handleUserOperationRevertReason(
   event: UserOperationRevertReasonEvent,
 ): void {
-  let entity = new UserOperationRevertReason(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  );
-  entity.userOpHash = event.params.userOpHash;
-  entity.sender = event.params.sender;
-  entity.nonce = event.params.nonce;
-  entity.revertReason = event.params.revertReason;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
+  // Handle if the account is LightWallet
+  handleLightWalletUserOperationRevertReason(event);
 }
 
-export function handleWithdrawn(event: WithdrawnEvent): void {
-  let entity = new Withdrawn(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  );
-  entity.account = event.params.account;
-  entity.withdrawAddress = event.params.withdrawAddress;
-  entity.amount = event.params.amount;
+// export function handleWithdrawn(event: WithdrawnEvent): void {
+//   let entity = new Withdrawn(
+//     event.transaction.hash.concatI32(event.logIndex.toI32()),
+//   );
+//   entity.account = event.params.account;
+//   entity.withdrawAddress = event.params.withdrawAddress;
+//   entity.amount = event.params.amount;
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+//   entity.blockNumber = event.block.number;
+//   entity.blockTimestamp = event.block.timestamp;
+//   entity.transactionHash = event.transaction.hash;
 
-  entity.save();
-}
+//   entity.save();
+// }
--- apps/subgraph/src/light-wallet.ts
+++ apps/subgraph/src/light-wallet.ts
@@ -14,9 +14,17 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { Address, Bytes } from "@graphprotocol/graph-ts";
-import { AccountDeployed as AccountDeployedEvent } from "../generated/EntryPointv0.6.0/EntryPoint";
+import {
+  AccountDeployed as AccountDeployedEvent,
+  UserOperationEvent as UserOperationEventEvent,
+  UserOperationRevertReason as UserOperationRevertReasonEvent,
+} from "../generated/EntryPointv0.6.0/EntryPoint";
 import { LightWallet as LightWaletInterface } from "../generated/EntryPointv0.6.0/LightWallet";
-import { LightWallet } from "../generated/schema";
+import {
+  LightWallet,
+  UserOperationEvent,
+  UserOperationRevertReason,
+} from "../generated/schema";
 
 export function handleLightWalletDeployed(event: AccountDeployedEvent): void {
   // If the event is emitted by one of the factories, then we know that the account is a LightWallet
@@ -25,23 +33,74 @@ export function handleLightWalletDeployed(event: AccountDeployedEvent): void {
     Address.fromString("0x0000000000756D3E6464f5efe7e413a0Af1C7474")
   ) {
     // Create a new LightWallet entity
-    let lightWallet = new LightWallet(event.params.userOpHash);
-    lightWallet.address = event.address;
+    let lightWallet = new LightWallet(event.params.sender);
+    lightWallet.address = event.params.sender;
+
     lightWallet.userOpHash = event.params.userOpHash;
     lightWallet.sender = event.params.sender;
     lightWallet.factory = event.params.factory;
     lightWallet.paymaster = event.params.paymaster;
+
     lightWallet.blockNumber = event.block.number;
     lightWallet.blockTimestamp = event.block.timestamp;
     lightWallet.transactionHash = event.transaction.hash;
 
     // Get the image hash of the LightWallet
-    let wallet = LightWaletInterface.bind(event.address);
+    let wallet = LightWaletInterface.bind(event.params.sender);
     let try_imageHash = wallet.try_imageHash();
     lightWallet.imageHash = try_imageHash.reverted
       ? new Bytes(0)
       : try_imageHash.value;
 
     lightWallet.save();
+  }
+}
+
+export function handleLightWalletUserOperationEvent(
+  event: UserOperationEventEvent,
+): void {
+  // Get the LightWallet entity
+  let lightWallet = LightWallet.load(event.params.sender);
+
+  // Handle if the account exists
+  if (lightWallet != null) {
+    let entity = new UserOperationEvent(event.params.userOpHash);
+    entity.userOpHash = event.params.userOpHash;
+    entity.sender = event.params.sender;
+    entity.paymaster = event.params.paymaster;
+    entity.nonce = event.params.nonce;
+    entity.success = event.params.success;
+    entity.actualGasCost = event.params.actualGasCost;
+    entity.actualGasUsed = event.params.actualGasUsed;
+
+    entity.blockNumber = event.block.number;
+    entity.blockTimestamp = event.block.timestamp;
+    entity.transactionHash = event.transaction.hash;
+
+    entity.save();
+  }
+}
+
+export function handleLightWalletUserOperationRevertReason(
+  event: UserOperationRevertReasonEvent,
+): void {
+  // Get the LightWallet entity
+  let lightWallet = LightWallet.load(event.params.sender);
+
+  // Handle if the account exists
+  if (lightWallet != null) {
+    let entity = new UserOperationRevertReason(
+      event.transaction.hash.concatI32(event.logIndex.toI32()),
+    );
+    entity.userOpHash = event.params.userOpHash;
+    entity.sender = event.params.sender;
+    entity.nonce = event.params.nonce;
+    entity.revertReason = event.params.revertReason;
+
+    entity.blockNumber = event.block.number;
+    entity.blockTimestamp = event.block.timestamp;
+    entity.transactionHash = event.transaction.hash;
+
+    entity.save();
   }
 }
