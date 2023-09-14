c5fb443e5f9b0920ea7a94d8bb0a71f9d117af6e
Shun Kakinoki <shunkakinoki@gmail.com>
2023-09-14 14:06:51 -0700

    chore: cleanup

--- crates/axum/src/routes/metrics.rs
+++ crates/axum/src/routes/metrics.rs
@@ -15,10 +15,14 @@
 
 use autometrics::{autometrics, prometheus_exporter};
 use axum::{response::IntoResponse, routing::get, Router};
+use http_body::Body as HttpBody;
 
-use crate::state::AppState;
-
-pub(crate) fn router() -> Router<AppState> {
+#[autometrics]
+pub(crate) fn router<S, B>() -> Router<S, B>
+where
+    B: HttpBody + Send + 'static,
+    S: Clone + Send + Sync + 'static,
+{
     Router::new().route("/metrics", get(handler))
 }
 
@@ -26,46 +30,3 @@ pub(crate) fn router() -> Router<AppState> {
 async fn handler() -> impl IntoResponse {
     prometheus_exporter::encode_http_response()
 }
-
-// From: https://github.com/get10101/10101/blob/a5f50e765c9a21ada284255fc2f742d01c4cba2f/maker/src/routes.rs#L245
-// License: MIT
-// #[autometrics]
-// pub async fn handler(State(state): State<AppState>) -> impl IntoResponse {
-//     // Get the autometrics.
-//     let autometrics = match autometrics::prometheus_exporter::encode_to_string() {
-//         Ok(metrics) => metrics,
-//         Err(err) => {
-//             error!("Could not collect autometrics {err:#}");
-//             return (StatusCode::INTERNAL_SERVER_ERROR, format!("{:?}", err));
-//         }
-//     };
-
-//     // Get the exporter and encoder.
-//     let encoder = TextEncoder::new();
-
-//     // Gather the metrics.
-//     let metric_families = prometheus::gather();
-
-//     // Return the metrics.
-//     let mut result = vec![];
-
-//     // Encode the metrics.
-//     match encoder.encode(&metric_families, &mut result) {
-//         Ok(()) => (),
-//         Err(err) => {
-//             error!("Could not collect opentelemetry metrics {err:#}");
-//             return (StatusCode::INTERNAL_SERVER_ERROR, format!("{:?}", err));
-//         }
-//     };
-
-//     // Convert the metrics to a string.
-//     let open_telemetry_metrics = match String::from_utf8(result) {
-//         Ok(s) => s,
-//         Err(err) => {
-//             error!("Could not format metrics as string {err:#}");
-//             return (StatusCode::INTERNAL_SERVER_ERROR, format!("{:?}", err));
-//         }
-//     };
-
-//     (StatusCode::OK, open_telemetry_metrics + &autometrics)
-// }
