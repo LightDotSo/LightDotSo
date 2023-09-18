9150f205afcffecb2263cd0fa6a7132a13acc8ff
Shun Kakinoki <shunkakinoki@gmail.com>
2023-09-17 20:46:00 -0700

    chore: update

--- crates/paymaster/src/lib.rs
+++ crates/paymaster/src/lib.rs
@@ -25,12 +25,3 @@ use ethers::{
     types::H256,
 };
 use eyre::Result;
-
-pub async fn sign() -> Result<()> {
-    let wallet: Wallet<SigningKey> =
-        "0000000000000000000000000000000000000000000000000000000000000001".parse().unwrap();
-    let wallet = wallet.with_chain_id(1_u64);
-
-    let _ = wallet.sign_message(H256::zero()).await?;
-    Ok(())
-}
