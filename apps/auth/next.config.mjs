7ca4249ad91fae27f68f191e23484f96b34a49af
Shun Kakinoki <shunkakinoki@gmail.com>
2023-09-21 16:31:53 -0700

    chore: update

--- apps/auth/next.config.mjs
+++ apps/auth/next.config.mjs
@@ -1,4 +1,24 @@
-const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { withSentryConfig } from "@sentry/nextjs";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
+const sentryWebpackPluginOptions = {
+  silent: false,
+};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -19,6 +39,7 @@ const nextConfig = {
   },
   async headers() {
     // To help with local development...
+    // eslint-disable-next-line no-undef
     if (process.env.NODE_ENV === "development") {
       return [
         {
@@ -95,4 +116,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
