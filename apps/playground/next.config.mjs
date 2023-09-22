9cbf658df05d5e107d49c41ffc5e24e3dd955b54
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-09-22 05:52:30 -0700

    feat: ini next sentry (#616)
    * feat: ini next sentry
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: ini playground env
    
    * chore: update
    
    * feat: ini sentry
    
    * chore: update

--- apps/playground/next.config.mjs
+++ apps/playground/next.config.mjs
@@ -1,4 +1,24 @@
-const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+import { withSentryConfig } from "@sentry/nextjs";
+
+const sentryWebpackPluginOptions = {
+  silent: false,
+};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -41,4 +61,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
