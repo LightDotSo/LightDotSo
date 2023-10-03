6ca952c8a594efe63f476f76cb2ce1781d2b2176
Shun Kakinoki <shunkakinoki@gmail.com>
2023-10-03 06:45:50 -0700

    feat: ini tabs

--- apps/app/src/components/MainNav.tsx
+++ apps/app/src/components/MainNav.tsx
@@ -13,49 +13,82 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import Link from "next/link";
+// Full complete example from: https://github.com/hqasmei/youtube-tutorials/blob/ee44df8fbf6ab4f4c2f7675f17d67813947a7f61/vercel-animated-tabs/src/components/tabs.tsx
+// License: MIT
 
+"use client";
+
+import React, { useState } from "react";
+
+import { Tabs } from "@/components/Tabs";
 import { cn } from "@lightdotso/ui";
+import { useTabs } from "@/hooks/useTabs";
+import {
+  DashboardIcon,
+  WidthIcon,
+  PersonIcon,
+  MixerVerticalIcon,
+  ChatBubbleIcon,
+} from "@radix-ui/react-icons";
+import type { IconProps } from "@radix-ui/react-icons/dist/types";
 
 export function MainNav({
-  className,
+  // eslint-disable-next-line react/prop-types
+  className = "",
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  const [hookProps] = useState({
+    tabs: [
+      {
+        label: "Overview",
+        id: "Overview",
+        href: "/",
+        number: 3,
+        icon: (
+          props: React.JSX.IntrinsicAttributes &
+            IconProps &
+            React.RefAttributes<SVGSVGElement>,
+        ) => <DashboardIcon {...props} />,
+      },
+      {
+        label: "Transactions",
+        id: "Transactions",
+        href: "/",
+        number: 3,
+        icon: props => <WidthIcon {...props} />,
+      },
+      {
+        label: "Members",
+        id: "Members",
+        href: "/",
+        number: 3,
+        icon: props => <PersonIcon {...props} />,
+      },
+      {
+        label: "Settings",
+        id: "Settings",
+        href: "/",
+        number: 3,
+        icon: props => <MixerVerticalIcon {...props} />,
+      },
+      {
+        label: "Support",
+        id: "Support",
+        href: "/",
+        number: 3,
+        icon: props => <ChatBubbleIcon {...props} />,
+      },
+    ],
+    initialTabId: "Overview",
+  });
+  const framer = useTabs(hookProps);
+
   return (
     <nav
       className={cn("flex items-center space-x-4 lg:space-x-6", className)}
       {...props}
     >
-      <Link
-        href="/"
-        className="text-sm font-medium transition-colors hover:text-primary"
-      >
-        Overview
-      </Link>
-      <Link
-        href="/"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Transactions
-      </Link>
-      <Link
-        href="/"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Members
-      </Link>
-      <Link
-        href="/"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Settings
-      </Link>
-      <Link
-        href="/"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Support
-      </Link>
+      <Tabs {...framer.tabProps} />
     </nav>
   );
 }
