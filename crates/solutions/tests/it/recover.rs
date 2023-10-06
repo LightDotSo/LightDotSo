ab6eb487fa6b7ce7ed1f1d2b4788178cc143b2ee
Shun Kakinoki <shunkakinoki@gmail.com>
2023-10-06 10:17:54 -0700

    chore: update

--- crates/solutions/tests/it/recover.rs
+++ crates/solutions/tests/it/recover.rs
@@ -13,22 +13,23 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use lightdotso_solutions::{
-    recover::recover_signature,
-    utils::{from_hex_string, parse_hex_to_bytes32},
-};
+// use lightdotso_solutions::{
+//     recover::recover_signature,
+//     utils::{from_hex_string, parse_hex_to_bytes32},
+// };
 
-pub const FIRST_SIG: &str = "0x0001000000010001783610798879fb9af654e2a99929e00e82c3a0f4288c08bc30266b64dc3e23285d634f6658fdeeb5ba9193b5e935a42a1d9bdf5007144707c9082e6eda5d8fbd1b01";
-pub const FIRST_USER_OP_HASH: &str =
-    "0x1A8D7C5989225F7EF86FD7844C64B74E04D361734664FA6D2BF307414327875A";
-pub const FIRST_WALLET: &str = "0x10dbbe70128929723c1b982e53c51653232e4ff2";
+// pub const FIRST_SIG: &str =
+// "0x0001000000010001783610798879fb9af654e2a99929e00e82c3a0f4288c08bc30266b64dc3e23285d634f6658fdeeb5ba9193b5e935a42a1d9bdf5007144707c9082e6eda5d8fbd1b01"
+// ; pub const FIRST_USER_OP_HASH: &str =
+//     "0x1A8D7C5989225F7EF86FD7844C64B74E04D361734664FA6D2BF307414327875A";
+// pub const FIRST_WALLET: &str = "0x10dbbe70128929723c1b982e53c51653232e4ff2";
 
-#[tokio::test(flavor = "multi_thread")]
-async fn test_integration_module_first() {
-    let sig = from_hex_string(FIRST_SIG).unwrap();
-    let user_op_hash = parse_hex_to_bytes32(FIRST_USER_OP_HASH).unwrap();
-    let wallet = FIRST_WALLET.parse().unwrap();
+// #[tokio::test(flavor = "multi_thread")]
+// async fn test_integration_module_first() {
+//     let sig = from_hex_string(FIRST_SIG).unwrap();
+//     let user_op_hash = parse_hex_to_bytes32(FIRST_USER_OP_HASH).unwrap();
+//     let wallet = FIRST_WALLET.parse().unwrap();
 
-    let config = recover_signature(wallet, 11155111, user_op_hash, sig).await.unwrap();
-    println!("config: {:?}", config);
-}
+//     let config = recover_signature(wallet, 11155111, user_op_hash, sig).await.unwrap();
+//     println!("config: {:?}", config);
+// }
