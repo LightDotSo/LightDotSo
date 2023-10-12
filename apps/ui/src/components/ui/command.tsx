745a2781050a97709da39870f8f51e1524d23f48
Shun Kakinoki <shunkakinoki@gmail.com>
2023-10-12 14:44:56 -0700

    perf: ini admin power

--- apps/ui/src/components/ui/command.tsx
+++ apps/ui/src/components/ui/command.tsx
@@ -61,7 +61,7 @@ const CommandInput = React.forwardRef<
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
-        "flex h-8 w-full rounded-md bg-transparent py-4.5 m-1.5 dark:focus:ring-offset-black dark:focus:border-indigo-1 focus:ring-offset-2 dark:focus:ring-indigo-2 text-sm placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+        "flex h-10 w-full rounded-md bg-transparent py-1.5 my-2 mx-1.5 dark:focus:ring-offset-black dark:focus:border-indigo-1 focus:ring-offset-2 dark:focus:ring-indigo-2 text-sm placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
