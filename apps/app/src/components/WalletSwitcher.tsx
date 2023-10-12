3b9f6474b3c0f0c079d8147de1b4d73d71ddea85
Shun Kakinoki <shunkakinoki@gmail.com>
2023-10-12 00:00:36 -0700

    chore: update

--- apps/app/src/components/WalletSwitcher.tsx
+++ apps/app/src/components/WalletSwitcher.tsx
@@ -122,7 +122,7 @@ export function WalletSwitcher({
             <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
+        <PopoverContent className="w-[300px] p-0">
           <Command>
             <CommandList>
               <CommandInput placeholder="Search wallet..." />
