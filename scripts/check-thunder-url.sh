84e3f0ca74f3e91c7bf843b7c728fb9cf1609e0a
Shun Kakinoki <shunkakinoki@gmail.com>
2023-10-12 14:58:17 -0700

    chore: update

--- scripts/check-thunder-url.sh
+++ scripts/check-thunder-url.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-URLS_TO_REJECT=('infura.io' 'alchemy.com' 'noderpc.xyz' 'pimlico.io')
+SLUGS_TO_REJECT=('infura.io' 'alchemy.com' 'noderpc.xyz' 'pimlico.io' 'Bearer')
 
 FILES=$(git diff --cached --name-only --diff-filter=ACM)
 for FILE in $FILES
 do
   if [[ $FILE == thunder-tests/* ]]
   then
-    for URL_TO_REJECT in "${URLS_TO_REJECT[@]}"
+    for SLUG_TO_REJECT in "${SLUGS_TO_REJECT[@]}"
     do
-      if grep -q $URL_TO_REJECT $FILE
+      if grep -q $SLUG_TO_REJECT $FILE
       then
-        echo "Rejected due to forbidden URL in file: $FILE"
+        echo "Rejected due to forbidden slug in file: $FILE"
         exit 1
       fi
     done
