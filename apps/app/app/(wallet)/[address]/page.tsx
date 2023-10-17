6fa3775e88e95c5658aef78695d5012ba98ab50b
Shun Kakinoki <shunkakinoki@gmail.com>
2023-10-16 22:42:53 -0700

    chore: update

--- apps/app/app/(wallet)/[address]/page.tsx
+++ apps/app/app/(wallet)/[address]/page.tsx
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Dashboard } from "@/components/tremor-dashboard";
 import { handler } from "@/handles/[address]";
 import { getLlama } from "@lightdotso/client";
 
@@ -30,7 +29,6 @@ export default async function Page({
 
   return (
     <div>
-      <Dashboard />
       <pre>
         <code>{JSON.stringify(res, null, 2)}</code>
       </pre>
