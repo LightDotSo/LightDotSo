ebcdec49cc8061ca5883ca0efba8f5cd4515de2a
Shun Kakinoki <shunkakinoki@gmail.com>
2023-10-18 03:31:18 -0700

    chore: update

--- scripts/add_recursive_flag.sh
+++ scripts/add_recursive_flag.sh
@@ -4,7 +4,7 @@ file_path="crates/prisma/src/lib.rs"
 temp_file="temp.rs"
 
 # Create a temporary file with the desired line at the top
-echo "#![recursion_limit = "256"]" > "$temp_file"
+echo '#![recursion_limit = "256"]' > "$temp_file"
 cat "$file_path" >> "$temp_file"
 
 # Overwrite the original file with the temporary file
