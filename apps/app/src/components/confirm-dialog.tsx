efdb30edd4138d9374f34d85c9966103c2f2a496
Shun Kakinoki <shunkakinoki@gmail.com>
2023-10-19 16:03:00 -0700

    perf: ini tx dialog

--- apps/app/src/components/confirm-dialog.tsx
+++ apps/app/src/components/confirm-dialog.tsx
@@ -15,9 +15,10 @@
 
 "use client";
 
-import { Button } from "@lightdotso/ui";
-import type { Address } from "viem";
+import { Button, toast } from "@lightdotso/ui";
+import { toBytes, type Address, toHex } from "viem";
 import { useCallback } from "react";
+import { sendUserOperation } from "@lightdotso/client";
 
 type ConfirmDialogProps = {
   address: Address;
@@ -53,7 +54,15 @@ type ConfirmDialogProps = {
       signature_type: number;
     }[];
   };
-  // userOpHash: Uint8Array;
+};
+
+const uint16ToUint8Array = (num: number) => {
+  return new Uint8Array([
+    // Get the first 8 bits by using a bitwise AND with "1111111100000000" and a bitwise right shift
+    (num & 0xff00) >> 8,
+    // Get the last 8 bits by using a bitwise AND with "0000000011111111"
+    num & 0x00ff,
+  ]);
 };
 
 export function ConfirmDialog({
@@ -75,9 +84,69 @@ export function ConfirmDialog({
     }, 0) >= (config ? config.threshold : 0);
 
   // A `useCallback` handler for confirming the operation
-  const handleConfirm = useCallback(() => {
-    // Construct the signature object
-  }, []);
+  const handleConfirm = useCallback(async () => {
+    // Construct the signature object in Uint8Array format
+
+    // For each signature in the userOperation signatures array and concatenate into one Uint8Array
+    //  - Get the owner from the config owners array
+    //  - Get the owner weight
+    const signatureArray = userOperation.signatures.flatMap(signature => {
+      const owner = config.owners.find(
+        owner => owner.id === signature?.owner_id,
+      );
+      const weight = owner?.weight || 0;
+
+      // Return the signature in Uint8Array format
+      return [
+        ...uint16ToUint8Array(weight),
+        ...Array.from(toBytes(signature.signature)),
+      ];
+    });
+
+    const signature = new Uint8Array(signatureArray);
+
+    // Prepend the threshold and signature to the userOperation signatures array
+    const pre_sig = new Uint8Array([
+      // Default to legacy signature type if not provided
+      // Assumes that all signatures are of the same type
+
+      // uint16
+      ...uint16ToUint8Array(config.threshold),
+      // uint32
+      ...uint16ToUint8Array(config.checkpoint),
+      // uint16
+      ...uint16ToUint8Array(userOperation.signatures[0]?.signature_type || 0),
+    ]);
+    // Concatenate the pre_sig and signature arrays
+    const sig = new Uint8Array([...pre_sig, ...signature]);
+
+    // Sned the user operation
+    const res = await sendUserOperation(chainId, [
+      {
+        sender: userOperation.sender,
+        nonce: toHex(userOperation.nonce),
+        initCode: userOperation.init_code,
+        callData: userOperation.call_data,
+        paymasterAndData: userOperation.paymaster_and_data,
+        callGasLimit: toHex(userOperation.call_gas_limit),
+        verificationGasLimit: toHex(userOperation.verification_gas_limit),
+        preVerificationGas: toHex(userOperation.pre_verification_gas),
+        maxFeePerGas: toHex(userOperation.max_fee_per_gas),
+        maxPriorityFeePerGas: toHex(userOperation.max_priority_fee_per_gas),
+        signature: toHex(sig),
+      },
+      "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
+    ]);
+
+    toast({
+      title: "You submitted the userOperation result",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(res, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }, [chainId, config, userOperation]);
 
   return (
     <>
@@ -110,3 +179,10 @@ export function ConfirmDialog({
     </>
   );
 }
+
+// "0x000100000001000193cf3925dde13d85c1abd3d0f5eaa8e46f97a66b0c390ac81abfaffeb84e648b0e1bc8504e0341e42158c0929798bd4cf45a88249fdceff2ad436bd013c678271b02"
+// "0x000100000001000186b1b39f492dcf87a23f645c8d9c58ba2dd51368b3418ddc422575375aca640c0a82c5eda9ca6a9313c0c59c4dd533d13fd6e7f54b2e927965ea05eeeb43b1f21b01"
+// "0x0001000000010001783610798879fb9af654e2a99929e00e82c3a0f4288c08bc30266b64dc3e23285d634f6658fdeeb5ba9193b5e935a42a1d9bdf5007144707c9082e6eda5d8fbd1b01"
+// "0x00010000000100018676f4db87420ca3de1fdb1f6a513924ef36570bb11b5ba345c22fd2fe430f672890496b0ede044dcd7a17c1d2d3e0e31808a9fedadeca920f368322ab6804d01c01"
+// "0x0001000000010001783610798879fb9af654e2a99929e00e82c3a0f4288c08bc30266b64dc3e23285d634f6658fdeeb5ba9193b5e935a42a1d9bdf5007144707c9082e6eda5d8fbd1b01"
+// "0x00010000000100018fb2dd47b62fa9e408827e63ea90c6a6607a42da44f2e497fc2ef75653b2873b61541c29124faa87fe3789ff9249420f937e678bb58a68693c6eeb6e068be2791b02"
