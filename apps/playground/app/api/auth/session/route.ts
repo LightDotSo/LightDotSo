0413cbbba6774a53a8178a6835406dbbf5ee9289
Shun Kakinoki <shunkakinoki@gmail.com>
2023-10-27 01:16:00 -0400

    chore: update checks

---
+++ apps/playground/app/api/auth/session/route.ts
@@ -0,0 +1,27 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "@lightdotso/next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+
+  return NextResponse.json({
+    authenticated: !!session,
+    session,
+  });
+}
