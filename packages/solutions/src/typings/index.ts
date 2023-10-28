118f94b90871bfb0e82b4179d332950df6bd8092
Shun Kakinoki <shunkakinoki@gmail.com>
2023-10-27 20:26:26 -0400

    chore: update

--- packages/solutions/src/typings/index.ts
+++ packages/solutions/src/typings/index.ts
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-export type { SignatureType, SignaturePartType } from "./signature";
+export { SignatureType, SignaturePartType } from "./signature";
 export type { Signer } from "./signer";
 export type {
   SignatureLeaf,
