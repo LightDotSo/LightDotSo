85539c6d2367359e6c62dab2c9927c61fe703db9
Shun Kakinoki <shunkakinoki@gmail.com>
2023-10-31 12:27:21 -0400

    chore: update

--- crates/paymaster/tests/it/main.rs
+++ crates/paymaster/tests/it/main.rs
@@ -196,4 +196,25 @@ async fn test_get_hash_iteration() {
 
     test_get_compare_user_operation(chain_id, verifying_paymaster_address, user_operation.clone())
         .await;
+
+    // Arbitrary test inputs #5
+    let chain_id = 137;
+    let verifying_paymaster_address: Address =
+        "0x000000000003193FAcb32D1C120719892B7AE977".parse().unwrap();
+    let user_operation = UserOperationConstruct {
+        // sender: "0x4fd9D0eE6D6564E80A9Ee00c0163fC952d0A45Ed".parse().unwrap(),
+        sender: "0x4fd9D0eE6D6564E80A9Ee00c0163fC952d0A4000".parse().unwrap(),
+        nonce: U256::from(1),
+        init_code: "0x0000000000756d3e6464f5efe7e413a0af1c7474183815c83c01efabf2ce62868626005b468fcc0cd03c644030e51dad0d5df74b0fbd4e950000000000000000000000000000000000000000000000000000018b838a07580000000000756d3e6464f5efe7e413a0af1c7474183815c83c01efabf2ce62868626005b468fcc0cd03c644030e51dad0d5df74b0fbd4e950000000000000000000000000000000000000000000000000000018b838a0758".parse().unwrap(),
+        call_data: "0x0000000000756d3e6464f5efe7e413a0af1c7474183815c83c01efabf2ce62868626005b468fcc0cd03c644030e51dad0d5df74b0fbd4e950000000000000000000000000000000000000000000000000000018b838a0758aaaa".parse().unwrap(),
+        call_gas_limit: U256::from(4514240),
+        verification_gas_limit: U256::from(1854272),
+        pre_verification_gas: U256::from(1854272),
+        max_fee_per_gas: U256::from(56674171701_i64),
+        max_priority_fee_per_gas: U256::from(48087546673_i64),
+        signature: "0x".parse().unwrap(),
+    };
+
+    test_get_compare_user_operation(chain_id, verifying_paymaster_address, user_operation.clone())
+        .await;
 }
