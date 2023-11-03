62495d2bb264d95a802e41c81c79db95de780f97
Shun Kakinoki <shunkakinoki@gmail.com>
2023-11-03 02:24:35 -0400

    feat: ini graphql polling

--- crates/graphql/src/polling/user_operations.rs
+++ crates/graphql/src/polling/user_operations.rs
@@ -66,6 +66,37 @@ pub struct UserOperation {
     pub user_operation_event: Option<UserOperationEvent>,
     pub user_operation_revert_reason: Option<UserOperationRevertReason>,
     pub light_wallet: LightWallet,
+    pub transaction: Transaction,
+    pub logs: Option<Vec<Log>>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct Transaction {
+    pub id: Bytes,
+    pub hash: Option<Bytes>,
+    pub index: Option<BigInt>,
+    pub from: Bytes,
+    pub to: Option<Bytes>,
+    pub value: Option<BigInt>,
+    pub gas_limit: Option<BigInt>,
+    pub gas_price: Option<BigInt>,
+    pub input: Option<Bytes>,
+    pub nonce: Option<BigInt>,
+    pub receipt: Option<Receipt>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct Receipt {
+    pub id: Bytes,
+    pub transaction_hash: Bytes,
+    pub transaction_index: BigInt,
+    pub block_hash: Bytes,
+    pub block_number: BigInt,
+    pub cumulative_gas_used: BigInt,
+    pub gas_used: BigInt,
+    pub status: BigInt,
+    pub logs_bloom: Bytes,
+    pub logs: Option<Vec<Log>>,
 }
 
 #[derive(cynic::QueryFragment, Debug)]
@@ -90,6 +121,19 @@ pub struct UserOperationEvent {
 }
 
 #[derive(cynic::QueryFragment, Debug)]
+pub struct Log {
+    pub id: cynic::Id,
+    pub address: Bytes,
+    pub topics: Option<Vec<Bytes>>,
+    pub data: Bytes,
+    pub block_hash: Bytes,
+    pub block_number: Bytes,
+    pub transaction_hash: Bytes,
+    pub transaction_index: BigInt,
+    pub log_index: BigInt,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
 pub struct LightWallet {
     pub id: Bytes,
     pub index: BigInt,
@@ -102,6 +146,139 @@ pub struct LightWallet {
     pub block_number: BigInt,
     pub block_timestamp: BigInt,
     pub transaction_hash: Bytes,
+}
+
+#[derive(cynic::Enum, Clone, Copy, Debug)]
+#[cynic(graphql_type = "UserOperation_orderBy")]
+pub enum UserOperationOrderBy {
+    #[cynic(rename = "id")]
+    Id,
+    #[cynic(rename = "index")]
+    Index,
+    #[cynic(rename = "sender")]
+    Sender,
+    #[cynic(rename = "nonce")]
+    Nonce,
+    #[cynic(rename = "initCode")]
+    InitCode,
+    #[cynic(rename = "callData")]
+    CallData,
+    #[cynic(rename = "callGasLimit")]
+    CallGasLimit,
+    #[cynic(rename = "verificationGasLimit")]
+    VerificationGasLimit,
+    #[cynic(rename = "preVerificationGas")]
+    PreVerificationGas,
+    #[cynic(rename = "maxFeePerGas")]
+    MaxFeePerGas,
+    #[cynic(rename = "maxPriorityFeePerGas")]
+    MaxPriorityFeePerGas,
+    #[cynic(rename = "paymasterAndData")]
+    PaymasterAndData,
+    #[cynic(rename = "signature")]
+    Signature,
+    #[cynic(rename = "blockNumber")]
+    BlockNumber,
+    #[cynic(rename = "blockTimestamp")]
+    BlockTimestamp,
+    #[cynic(rename = "transactionHash")]
+    TransactionHash,
+    #[cynic(rename = "entryPoint")]
+    EntryPoint,
+    #[cynic(rename = "lightWallet")]
+    LightWallet,
+    #[cynic(rename = "lightWallet__id")]
+    LightWalletId,
+    #[cynic(rename = "lightWallet__index")]
+    LightWalletIndex,
+    #[cynic(rename = "lightWallet__address")]
+    LightWalletAddress,
+    #[cynic(rename = "lightWallet__imageHash")]
+    LightWalletImageHash,
+    #[cynic(rename = "lightWallet__userOpHash")]
+    LightWalletUserOpHash,
+    #[cynic(rename = "lightWallet__sender")]
+    LightWalletSender,
+    #[cynic(rename = "lightWallet__factory")]
+    LightWalletFactory,
+    #[cynic(rename = "lightWallet__paymaster")]
+    LightWalletPaymaster,
+    #[cynic(rename = "lightWallet__blockNumber")]
+    LightWalletBlockNumber,
+    #[cynic(rename = "lightWallet__blockTimestamp")]
+    LightWalletBlockTimestamp,
+    #[cynic(rename = "lightWallet__transactionHash")]
+    LightWalletTransactionHash,
+    #[cynic(rename = "logs")]
+    Logs,
+    #[cynic(rename = "transaction")]
+    Transaction,
+    #[cynic(rename = "transaction__id")]
+    TransactionId,
+    #[cynic(rename = "transaction__hash")]
+    _TransactionHash,
+    #[cynic(rename = "transaction__index")]
+    TransactionIndex,
+    #[cynic(rename = "transaction__from")]
+    TransactionFrom,
+    #[cynic(rename = "transaction__to")]
+    TransactionTo,
+    #[cynic(rename = "transaction__value")]
+    TransactionValue,
+    #[cynic(rename = "transaction__gasLimit")]
+    TransactionGasLimit,
+    #[cynic(rename = "transaction__gasPrice")]
+    TransactionGasPrice,
+    #[cynic(rename = "transaction__input")]
+    TransactionInput,
+    #[cynic(rename = "transaction__nonce")]
+    TransactionNonce,
+    #[cynic(rename = "userOperationEvent")]
+    UserOperationEvent,
+    #[cynic(rename = "userOperationEvent__id")]
+    UserOperationEventId,
+    #[cynic(rename = "userOperationEvent__index")]
+    UserOperationEventIndex,
+    #[cynic(rename = "userOperationEvent__userOpHash")]
+    UserOperationEventUserOpHash,
+    #[cynic(rename = "userOperationEvent__sender")]
+    UserOperationEventSender,
+    #[cynic(rename = "userOperationEvent__paymaster")]
+    UserOperationEventPaymaster,
+    #[cynic(rename = "userOperationEvent__nonce")]
+    UserOperationEventNonce,
+    #[cynic(rename = "userOperationEvent__success")]
+    UserOperationEventSuccess,
+    #[cynic(rename = "userOperationEvent__actualGasCost")]
+    UserOperationEventActualGasCost,
+    #[cynic(rename = "userOperationEvent__actualGasUsed")]
+    UserOperationEventActualGasUsed,
+    #[cynic(rename = "userOperationEvent__blockNumber")]
+    UserOperationEventBlockNumber,
+    #[cynic(rename = "userOperationEvent__blockTimestamp")]
+    UserOperationEventBlockTimestamp,
+    #[cynic(rename = "userOperationEvent__transactionHash")]
+    UserOperationEventTransactionHash,
+    #[cynic(rename = "userOperationRevertReason")]
+    UserOperationRevertReason,
+    #[cynic(rename = "userOperationRevertReason__id")]
+    UserOperationRevertReasonId,
+    #[cynic(rename = "userOperationRevertReason__index")]
+    UserOperationRevertReasonIndex,
+    #[cynic(rename = "userOperationRevertReason__userOpHash")]
+    UserOperationRevertReasonUserOpHash,
+    #[cynic(rename = "userOperationRevertReason__sender")]
+    UserOperationRevertReasonSender,
+    #[cynic(rename = "userOperationRevertReason__nonce")]
+    UserOperationRevertReasonNonce,
+    #[cynic(rename = "userOperationRevertReason__revertReason")]
+    UserOperationRevertReasonRevertReason,
+    #[cynic(rename = "userOperationRevertReason__blockNumber")]
+    UserOperationRevertReasonBlockNumber,
+    #[cynic(rename = "userOperationRevertReason__blockTimestamp")]
+    UserOperationRevertReasonBlockTimestamp,
+    #[cynic(rename = "userOperationRevertReason__transactionHash")]
+    UserOperationRevertReasonTransactionHash,
 }
 
 #[derive(cynic::Scalar, Debug, Clone)]
