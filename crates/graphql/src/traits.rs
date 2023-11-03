d10840104202755ecb31476a1aa06c04a3c87c28
Shun Kakinoki <shunkakinoki@gmail.com>
2023-11-03 02:58:57 -0400

    perf: ini trait construct

--- crates/graphql/src/traits.rs
+++ crates/graphql/src/traits.rs
@@ -17,31 +17,42 @@ use crate::polling::user_operations::UserOperation;
 use lightdotso_common::traits::HexToBytes;
 use lightdotso_prisma::{user_operation, UserOperationStatus};
 
-// Implement From<UserOperation> for User operation.
-impl From<UserOperation> for user_operation::Data {
-    fn from(user_operation: UserOperation) -> Self {
+pub struct UserOperationConstruct {
+    pub user_operation: UserOperation,
+    pub chain_id: i64,
+}
+
+// Implement From<UserOperationConstruct> for User operation.
+impl From<UserOperationConstruct> for user_operation::Data {
+    fn from(op: UserOperationConstruct) -> Self {
         Self {
-            chain_id: 0,
-            hash: user_operation.id.0.parse().unwrap(),
-            sender: user_operation.sender.0.parse().unwrap(),
-            nonce: user_operation.nonce.0.parse().unwrap(),
-            init_code: user_operation.init_code.clone().0.hex_to_bytes().unwrap(),
-            call_data: user_operation.call_data.clone().0.hex_to_bytes().unwrap(),
-            call_gas_limit: user_operation.call_gas_limit.0.parse().unwrap(),
-            verification_gas_limit: user_operation.verification_gas_limit.0.parse().unwrap(),
-            pre_verification_gas: user_operation.pre_verification_gas.0.parse().unwrap(),
-            max_fee_per_gas: user_operation.max_fee_per_gas.0.parse().unwrap(),
-            max_priority_fee_per_gas: user_operation.max_priority_fee_per_gas.0.parse().unwrap(),
-            paymaster_and_data: user_operation.paymaster_and_data.clone().0.hex_to_bytes().unwrap(),
+            chain_id: op.chain_id,
+            hash: op.user_operation.id.0.parse().unwrap(),
+            sender: op.user_operation.sender.0.parse().unwrap(),
+            nonce: op.user_operation.nonce.0.parse().unwrap(),
+            init_code: op.user_operation.init_code.clone().0.hex_to_bytes().unwrap(),
+            call_data: op.user_operation.call_data.clone().0.hex_to_bytes().unwrap(),
+            call_gas_limit: op.user_operation.call_gas_limit.0.parse().unwrap(),
+            verification_gas_limit: op.user_operation.verification_gas_limit.0.parse().unwrap(),
+            pre_verification_gas: op.user_operation.pre_verification_gas.0.parse().unwrap(),
+            max_fee_per_gas: op.user_operation.max_fee_per_gas.0.parse().unwrap(),
+            max_priority_fee_per_gas: op.user_operation.max_priority_fee_per_gas.0.parse().unwrap(),
+            paymaster_and_data: op
+                .user_operation
+                .paymaster_and_data
+                .clone()
+                .0
+                .hex_to_bytes()
+                .unwrap(),
             status: UserOperationStatus::Executed,
-            signature: user_operation.signature.clone().0.hex_to_bytes().unwrap().into(),
-            entry_point: user_operation.entry_point.0.parse().unwrap(),
+            signature: op.user_operation.signature.clone().0.hex_to_bytes().unwrap().into(),
+            entry_point: op.user_operation.entry_point.0.parse().unwrap(),
             paymaster: None,
             paymaster_id: None,
             transaction: None,
             transaction_hash: None,
             wallet: None,
-            wallet_address: user_operation.light_wallet.id.0.parse().unwrap(),
+            wallet_address: op.user_operation.light_wallet.id.0.parse().unwrap(),
             notification: None,
             notification_id: None,
             history: None,
