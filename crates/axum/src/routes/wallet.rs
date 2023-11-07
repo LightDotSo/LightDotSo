1ceb6b1542bb83db09a3d54873f3830fb7d0352e
Shun Kakinoki <shunkakinoki@gmail.com>
2023-11-07 11:26:25 -0500

    chore: ini wallet creation user many

--- crates/axum/src/routes/wallet.rs
+++ crates/axum/src/routes/wallet.rs
@@ -419,40 +419,8 @@ async fn v1_wallet_post_handler(
         }));
     }
 
-    // Get the users that don't exist in the database.
-    let users = client
-        .clone()
-        .client
-        .unwrap()
-        .user()
-        .find_many(
-            owners
-                .iter()
-                .map(|owner| {
-                    lightdotso_prisma::user::address::equals(Some(to_checksum(
-                        &owner.address.parse::<H160>().unwrap(),
-                        None,
-                    )))
-                })
-                .collect(),
-        )
-        .exec()
-        .await?;
-
-    // Parse the users into a vector of addresses.
-    let users: Vec<String> = users.iter().map(|user| user.address.clone().unwrap()).collect();
-    info!(?users);
-
-    // Get the owners that don't exist in the vector of addresses.
-    let owners: Vec<&Owner> = owners
-        .iter()
-        .filter(|owner| {
-            !users.contains(&to_checksum(&owner.address.parse::<H160>().unwrap(), None))
-        })
-        .collect();
-    info!("{}", owners.len());
-
     // Attempt to create a user in case it does not exist.
+    // If the user already exists, it will be skipped.
     let res = client
         .clone()
         .client
@@ -471,6 +439,7 @@ async fn v1_wallet_post_handler(
                 })
                 .collect(),
         )
+        .skip_duplicates()
         .exec()
         .await?;
     info!(?res);
