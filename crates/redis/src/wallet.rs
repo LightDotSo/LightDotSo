03ca7a95ee25587ef8d0be28d30dc2964a2f1e43
Shun Kakinoki <shunkakinoki@gmail.com>
2023-11-14 19:22:07 -0800

    fix: redis check

--- crates/redis/src/wallet.rs
+++ crates/redis/src/wallet.rs
@@ -18,23 +18,10 @@ use lightdotso_tracing::tracing::info;
 pub use redis;
 use redis::{Commands, Connection, RedisResult};
 
-/// Determine shard key based on the first 2 letters of the hex address
-fn get_wallet_shard_key(address: &str) -> String {
-    // Ensure the address is long enough for sharding
-    if address.len() < 2 {
-        return format!("{}:default", WALLET.as_str());
-    }
-    let shard_identifier = &address[0..2];
-    format!("{}:{}", WALLET.as_str(), shard_identifier)
-}
-
 /// Add a value to a set
 pub fn add_to_wallets(con: &mut Connection, value: &str) -> RedisResult<()> {
-    // Determine the shard key
-    let key = get_wallet_shard_key(value);
-
     // Add the value to the set
-    con.sadd(key, value)?;
+    con.sadd(WALLET.as_str(), value)?;
 
     // Return Ok
     Ok(())
@@ -53,29 +40,9 @@ pub fn is_wallet_present(
 
     // Add the commands to the pipeline
     for member in members {
-        let key = get_wallet_shard_key(&member);
-        pipe.cmd("SISMEMBER").arg(key).arg(member);
+        pipe.cmd("SISMEMBER").arg(WALLET.as_str()).arg(member);
     }
 
     // Execute the pipeline
     pipe.query(con)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_get_wallet_shard_key_empty_address() {
-        let address = "";
-        let shard_key = get_wallet_shard_key(address);
-        assert_eq!(shard_key, "wallet:default");
-    }
-
-    #[test]
-    fn test_get_wallet_shard_key_typical_hex() {
-        let address = "0xabcdef";
-        let shard_key = get_wallet_shard_key(address);
-        assert_eq!(shard_key, "wallet:0x");
-    }
 }
