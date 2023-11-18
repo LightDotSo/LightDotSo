9182ac3802979a083e1cb47a4cb6aac07b3dda73
Shun Kakinoki <shunkakinoki@gmail.com>
2023-11-17 17:33:07 -0800

    chore: update

--- apps/ui/.storybook/main.ts
+++ apps/ui/.storybook/main.ts
@@ -9,7 +9,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-styling",
-    "storybook-dark-mode"
+    "storybook-dark-mode",
   ],
   framework: {
     name: "@storybook/react-vite",
