b478df908639c9cd4b8a0d83218a0b8c652842aa
Shun Kakinoki <shunkakinoki@gmail.com>
2023-11-18 10:00:50 -0800

    chore: ini deprecate secondary

--- apps/ui/src/components/ui/button.tsx
+++ apps/ui/src/components/ui/button.tsx
@@ -29,8 +29,6 @@ const buttonVariants = cva(
           "bg-background-destructive text-text-inverse hover:bg-background-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
