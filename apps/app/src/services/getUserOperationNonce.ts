f0d8a24389fd047d5e1a66313396ff92cc8a0fe9
Shun Kakinoki <shunkakinoki@gmail.com>
2023-11-26 22:36:21 -0800

    chore: update

--- apps/app/src/services/getUserOperationNonce.ts
+++ apps/app/src/services/getUserOperationNonce.ts
@@ -32,9 +32,14 @@ export const preload = (address: Address, chain_id: number) => {
 export const getUserOperationNonce = async (
   address: Address,
   chain_id: number,
+  paymaster: Address = "0x000000000003193FAcb32D1C120719892B7AE977",
 ) => {
   return getClientUserOperationNonce(
-    { params: { query: { address: address, chain_id: chain_id } } },
+    {
+      params: {
+        query: { address: address, chain_id: chain_id, paymaster: paymaster },
+      },
+    },
     false,
   );
 };
