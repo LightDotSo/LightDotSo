08139b3ec1cd6459a6699d451af91e683884bc92
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-11-29 18:22:33 -0800

    chore: Chore/upd (#996)
    * chore: ini deploy
    
    * chore: update
    
    * chore: update
    
    * chore: update

--- crates/rpc/src/lib.rs
+++ crates/rpc/src/lib.rs
@@ -18,8 +18,9 @@ mod constants;
 
 use crate::constants::{
     ALCHEMY_RPC_URLS, ANKR_RPC_URLS, BLASTAPI_RPC_URLS, BUNDLER_RPC_URL, CHAINNODES_RPC_URLS,
-    GAS_RPC_URL, INFURA_RPC_URLS, LLAMANODES_RPC_URLS, NODEREAL_RPC_URLS, PAYMASTER_RPC_URL,
-    PIMLICO_RPC_URLS, PUBLIC_RPC_URLS, SIMULATOR_RPC_URL, THIRDWEB_RPC_URLS,
+    GAS_RPC_URL, INFURA_RPC_URLS, LLAMANODES_RPC_URLS, NODEREAL_RPC_URLS, OFFICIAL_PUBLIC_RPC_URLS,
+    PAYMASTER_RPC_URL, PIMLICO_RPC_URLS, PUBLIC_NODE_RPC_URLS, SIMULATOR_RPC_URL,
+    THIRDWEB_RPC_URLS,
 };
 use axum::{
     body::Body,
@@ -525,7 +526,20 @@ pub async fn rpc_proxy_handler(
 
     // Get the public rpc url from the constants
     let result = try_rpc_with_url(
-        &PUBLIC_RPC_URLS,
+        &PUBLIC_NODE_RPC_URLS,
+        None,
+        &chain_id,
+        &client,
+        Body::from(full_body_bytes.clone()),
+    )
+    .await;
+    if let Some(resp) = result {
+        return resp;
+    }
+
+    // Get the official public rpc url from the constants
+    let result = try_rpc_with_url(
+        &OFFICIAL_PUBLIC_RPC_URLS,
         None,
         &chain_id,
         &client,
