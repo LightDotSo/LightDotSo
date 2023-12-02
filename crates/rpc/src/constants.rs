ee0eb81a8201f96acbc0c0fba3a1e46b9cfafe95
Shun Kakinoki <shunkakinoki@gmail.com>
2023-12-01 19:27:37 -0800

    fix: return

--- crates/rpc/src/constants.rs
+++ crates/rpc/src/constants.rs
@@ -293,16 +293,18 @@ lazy_static! {
 }
 
 // The candide rpc urls
+#[allow(clippy::let_and_return)]
 lazy_static! {
     pub static ref CANDIDE_RPC_URLS: HashMap<u64, String> = {
-        let mut m = HashMap::new();
+        let m = HashMap::new();
+        // let mut m = HashMap::new();
 
         // Mainnet
         // m.insert(137, "https://polygon.voltaire.candidewallet.com/rpc".to_string());
 
          // Testnet
         // m.insert(44787, "https://celo-alfajores.voltaire.candidewallet.com/rpc".to_string());
-        m.insert(80001, "https://mumbai.voltaire.candidewallet.com/rpc".to_string());
+        // m.insert(80001, "https://mumbai.voltaire.candidewallet.com/rpc".to_string());
         // m.insert(11155111, "https://sepolia.voltaire.candidewallet.com/rpc".to_string());
 
         m
