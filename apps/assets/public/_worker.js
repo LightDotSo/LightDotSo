c8502601554c6f18b444b81194cff58390091ca6
Shun Kakinoki <shunkakinoki@gmail.com>
2023-12-08 07:08:21 -0800

    chore: update

---
+++ apps/assets/public/_worker.js
@@ -0,0 +1,32 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const objectName = url.pathname.slice(1);
+    if (request.method === "GET") {
+      const object = await env.ASSETS.get(objectName);
+      if (object === null) {
+        return new Response("Object Not Found", { status: 404 });
+      }
+      const headers = new Headers();
+      object.writeHttpMetadata(headers);
+      headers.set("etag", object.httpEtag);
+      return new Response(object.body, {
+        headers,
+      });
+    }
+  },
+};
