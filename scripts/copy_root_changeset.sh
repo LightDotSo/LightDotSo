88013ee44c948ed0bc16292654bbe54872b08ccf
Shun Kakinoki <shunkakinoki@gmail.com>
2023-12-08 13:45:26 -0800

    chore: update

--- scripts/copy_root_changeset.sh
+++ scripts/copy_root_changeset.sh
@@ -16,25 +16,30 @@ do
 
     echo "Description: ${description}"
 
+    # Use printf to properly format newline characters in the description
+    description=$(printf '%s' "$description")
+
     # Define the section based on version bump.
     case $version_bump in
       major)
-        section="Major changes"
+        section="## Major changes"
+        grep -q "${section}" ${target_file}  || echo -e "\n${section}\n" >> ${target_file}
         ;;
       minor)
-        section="Minor changes"
+        section="## Minor changes"
+        grep -q "${section}" ${target_file}  || echo -e "\n${section}\n" >> ${target_file}
         ;;
       patch)
-        section="Patch changes"
+        section="## Patch changes"
+        grep -q "${section}" ${target_file}  || echo -e "\n${section}\n" >> ${target_file}
         ;;
       *)
         echo "Invalid version bump: ${version_bump}"
         exit 1
     esac
 
-    # Use printf to properly format newline characters in the description
-    description=$(printf '%s' "$description")
-
     # Append the description under the correct section
-    echo -e "/## ${section}/a\n${description}\n.\nw\nq" | ed -s "${target_file}"
+    echo -e "/${section}/a\n Sorry for the previous incomplete response. Please find the completed version below:"
+
+    echo -e "/${section}/a\n${description}\n.\nw\nq" | ed -s "${target_file}"
 done
