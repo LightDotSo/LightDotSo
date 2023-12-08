1817baac868923c149d9621d507dacbb323e64b7
Shun Kakinoki <shunkakinoki@gmail.com>
2023-12-08 06:01:50 -0800

    chore: update

--- apps/ui/src/components/ui/button.tsx
+++ apps/ui/src/components/ui/button.tsx
@@ -20,7 +20,7 @@ import { Loader2 } from "lucide-react";
 import { forwardRef } from "react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-border-info focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-border-info focus-visible:ring-offset-2 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
   {
     variants: {
       variant: {
