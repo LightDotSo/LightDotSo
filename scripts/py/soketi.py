ddfd8a04188696f470664af12d433f213d24e4f7
Shun Kakinoki <shunkakinoki@gmail.com>
2023-12-17 15:55:05 -0800

    feat: ini pusher crate

--- scripts/py/soketi.py
+++ scripts/py/soketi.py
@@ -1,18 +1,21 @@
 import pusher
 
+
 def realtime_client():
     return pusher.Pusher(
-        app_id='333',
-        key='PUSHER_KEY',
-        secret='PUSHER_SECRET',
-        host='soketi.light.so',
+        app_id="333",
+        key="PUSHER_KEY",
+        secret="PUSHER_SECRET",
+        host="soketi.light.so",
         port=443,
         ssl=True,
     )
 
-def root_service(channel="test-channel", event='test-event'):
-    data = {"message":"alert message"}
+
+def root_service(channel="test-channel", event="test-event"):
+    data = {"message": "alert message"}
     realtime_client().trigger(channel, event, data)
+
 
 if __name__ == "__main__":
     root_service()
