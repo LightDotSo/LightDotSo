b6b87eed7df4df8a76e968ad4ca96ad693947c67
Shun Kakinoki <shunkakinoki@gmail.com>
2023-12-21 13:42:46 -0800

    chore: update types

--- apps/ui/src/components/ui/button.tsx
+++ apps/ui/src/components/ui/button.tsx
@@ -238,9 +238,9 @@ const buttonVariants = cva(
         ],
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "h-19 px-4 py-2",
+        sm: "h-8 px-3 py-1.5",
+        lg: "h-10 px-8",
         icon: "h-10 w-10",
         unsized: "",
       },
