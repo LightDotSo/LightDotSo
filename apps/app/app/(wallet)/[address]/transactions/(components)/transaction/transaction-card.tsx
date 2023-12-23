76ab11f76acba70663bff71771ff3313c907a247
Shun Kakinoki <shunkakinoki@gmail.com>
2023-12-23 03:58:13 -0800

    fix: tx card hash

--- apps/app/app/(wallet)/[address]/transactions/(components)/transaction/transaction-card.tsx
+++ apps/app/app/(wallet)/[address]/transactions/(components)/transaction/transaction-card.tsx
@@ -31,6 +31,7 @@ import {
 import { shortenBytes32 } from "@lightdotso/utils";
 import type { Row } from "@tanstack/react-table";
 import { flexRender } from "@tanstack/react-table";
+import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 import type { FC } from "react";
 import { useMemo } from "react";
@@ -38,6 +39,7 @@ import type { Address } from "viem";
 import { TransactionCardExecuteButton } from "@/app/(wallet)/[address]/transactions/(components)/transaction/transaction-card-execute-button";
 import { UserOperationTimeline } from "@/components/user-operation/user-operation-timeline";
 import type { ConfigurationData, UserOperationData } from "@/data";
+import { getChainById } from "@/utils";
 
 // -----------------------------------------------------------------------------
 // Props
@@ -49,6 +51,12 @@ type TransactionCardProps = {
   userOperation: UserOperationData;
   row: Row<UserOperationData>;
 };
+
+interface TransactionInformationItem {
+  title: string;
+  value: string | number;
+  href?: string;
+}
 
 // -----------------------------------------------------------------------------
 // Component
@@ -64,23 +72,55 @@ export const TransactionCard: FC<TransactionCardProps> = ({
   // Effect Hooks
   // ---------------------------------------------------------------------------
 
+  const chain = useMemo(
+    () => getChainById(userOperation.chain_id),
+    [userOperation],
+  );
+
   const informationItems = useMemo(() => {
-    const items = [
+    const items: TransactionInformationItem[] = [
       { title: "Hash", value: shortenBytes32(userOperation.hash) },
       { title: "Nonce", value: userOperation.nonce },
-      { title: "Status", value: userOperation.status.toLowerCase() },
+      {
+        title: "Status",
+        value:
+          userOperation.status.charAt(0) +
+          userOperation.status.slice(1).toLowerCase(),
+      },
       { title: "Signatures", value: userOperation.signatures.length },
+      { title: "Threshold", value: configuration?.threshold },
+      {
+        title: "Created At",
+        value: new Date(userOperation.created_at).toLocaleString(),
+      },
+      {
+        title: "Updated At",
+        value: new Date(userOperation.updated_at).toLocaleString(),
+      },
+      { title: "Chain", value: chain?.name },
     ];
 
     if (userOperation.transaction) {
       items.push({
-        title: "Transaction",
+        title: "Transaction Hash",
         value: shortenBytes32(userOperation.transaction.hash),
+        href: `${chain?.blockExplorers?.default?.url}/tx/${userOperation.transaction.hash}`,
       });
     }
 
     return items;
-  }, [userOperation]);
+  }, [
+    chain?.blockExplorers?.default?.url,
+    chain?.name,
+    configuration?.threshold,
+    userOperation.created_at,
+    userOperation.hash,
+    userOperation.nonce,
+    userOperation.signatures.length,
+    userOperation.status,
+    userOperation.transaction,
+    userOperation.updated_at,
+  ]);
 
   // ---------------------------------------------------------------------------
   // Render
@@ -105,7 +145,7 @@ export const TransactionCard: FC<TransactionCardProps> = ({
         <CollapsibleContent asChild>
           <TableCell className="p-0" colSpan={row.getAllCells().length}>
             <div className="m-4 grid gap-4 md:grid-cols-2">
-              <Card className="col-span-1 space-y-4 border border-border-weak bg-background-stronger p-4">
+              <Card className="col-span-1 space-y-4 border border-border-weak bg-background-strong p-4">
                 <CardHeader className="p-0">
                   <CardTitle className="text-lg">
                     Transaction Information
@@ -123,21 +163,41 @@ export const TransactionCard: FC<TransactionCardProps> = ({
                       <div>
                         <div className="text-text-weak">{item.title}</div>
                       </div>
-                      <div className="text-text">{item.value}</div>
+                      <div className="group flex items-center space-x-1.5 text-text">
+                        {item.href ? (
+                          <>
+                            <a
+                              href={item.href}
+                              target="_blank"
+                              rel="noreferrer"
+                              className="group-hover:underline"
+                            >
+                              {item.value}
+                            </a>
+                            <ArrowUpRight className="ml-2 h-4 w-4 shrink-0 opacity-50 group-hover:underline group-hover:opacity-100" />
+                          </>
+                        ) : (
+                          <>{item.value}</>
+                        )}
+                      </div>
                     </div>
                   ))}
                 </CardContent>
-                <CardFooter className="flex w-full items-center justify-end p-0">
-                  <Button asChild>
+                <CardFooter className="p-0">
+                  <Button
+                    asChild
+                    variant="ghost"
+                    className="w-full bg-background-stronger"
+                  >
                     <Link
                       href={`/${userOperation.sender}/op/${userOperation.hash}`}
                     >
-                      See More
+                      See Details
                     </Link>
                   </Button>
                 </CardFooter>
               </Card>
-              <Card className="col-span-1 space-y-4 border border-border-weak bg-background-stronger p-4">
+              <Card className="col-span-1 space-y-4 border border-border-weak bg-background-strong p-4">
                 <CardHeader className="p-0">
                   <CardTitle className="text-lg">Progress</CardTitle>
                   <CardDescription>
