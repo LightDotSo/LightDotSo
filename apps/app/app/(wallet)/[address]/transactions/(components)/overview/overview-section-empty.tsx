f92af6d56ce2f1847994f87d8dab03e553c2c0a6
Shun Kakinoki <shunkakinoki@gmail.com>
2023-12-28 15:52:32 -0800

    chore: update

--- apps/app/app/(wallet)/[address]/transactions/(components)/overview/overview-section-empty.tsx
+++ apps/app/app/(wallet)/[address]/transactions/(components)/overview/overview-section-empty.tsx
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"use client";
+
 import { Table, TableBody, TableCell, TableRow } from "@lightdotso/ui";
 import { useQueryClient } from "@tanstack/react-query";
 import type { Address } from "viem";
