07ce8e263ca38446cb285652d1905507261ec8cf
Shun Kakinoki <shunkakinoki@gmail.com>
2023-12-27 21:36:50 -0800

    chore: ini button icon

---
+++ apps/ui/src/components/ui/button-icon.stories.tsx
@@ -0,0 +1,48 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import type { Meta, StoryObj } from "@storybook/react";
+import { Mail } from "lucide-react";
+import { ButtonIcon } from "./button-icon";
+
+// -----------------------------------------------------------------------------
+// Meta
+// -----------------------------------------------------------------------------
+
+const meta: Meta<typeof ButtonIcon> = {
+  title: "ui/ButtonIcon",
+  component: ButtonIcon,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+export default meta;
+
+// -----------------------------------------------------------------------------
+// Types
+// -----------------------------------------------------------------------------
+
+type Story = StoryObj<typeof ButtonIcon>;
+
+// -----------------------------------------------------------------------------
+// Story
+// -----------------------------------------------------------------------------
+
+export const Base: Story = {
+  render: args => (
+    <ButtonIcon {...args}>
+      <Mail />
+    </ButtonIcon>
+  ),
+};
