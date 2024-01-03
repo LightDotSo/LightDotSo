24838111b3c60c087ca2d0b28afe95e61e98cac3
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-03 02:20:36 -0800

    chore: update

--- crates/prisma/src/main.rs
+++ crates/prisma/src/main.rs
@@ -13,6 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#![recursion_limit = "256"]
 #[allow(clippy::all)]
 
 /// Empty file to make `cargo run` work.
