a8f29b08e2a5741fcb470e7a52e8cd3b25609834
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-07 23:51:26 -0800

    fix: ses

--- crates/client/build.rs
+++ crates/client/build.rs
@@ -33,10 +33,7 @@ fn main() {
 
     let tokens = generator.generate_tokens(&spec).unwrap();
     let ast = syn::parse2(tokens).unwrap();
-
-    // Allow clippy::unwrap_used for the coming generated code
-    let mut content = "#![allow(clippy::all)]\n\n".to_owned();
-    content.push_str(&prettyplease::unparse(&ast));
+    let content = prettyplease::unparse(&ast);
 
     let mut out_file = Path::new(&env::var("OUT_DIR").unwrap()).to_path_buf();
     out_file.push("codegen.rs");
