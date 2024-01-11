9ed2b2f5c35d622e68e4b002931362340b9c054f
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-11 14:10:38 -0800

    chore: update

--- apps/app/src/components/nft/nft-image.tsx
+++ apps/app/src/components/nft/nft-image.tsx
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"use client";
+
 /* eslint-disable @next/next/no-img-element */
 
 import { cn } from "@lightdotso/utils";
