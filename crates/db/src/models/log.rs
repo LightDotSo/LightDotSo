5737484ba1beb336fe352c0509266f76bdbb76b9
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-10 22:40:35 -0800

    fix: log order

--- crates/db/src/models/log.rs
+++ crates/db/src/models/log.rs
@@ -1,4 +1,3 @@
-#![allow(clippy::unwrap_used)]
 // Copyright (C) 2023 Light, Inc.
 //
 // This program is free software: you can redistribute it and/or modify
@@ -14,6 +13,9 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#![allow(clippy::unwrap_used)]
+
+use eyre::{eyre, Result};
 use lightdotso_prisma::log;
 use serde::{Deserialize, Serialize};
 
@@ -33,18 +35,37 @@ impl TryFrom<log::Data> for DbLog {
     fn try_from(log: log::Data) -> Result<Self, Self::Error> {
         let topics = log.clone().topics.unwrap().into_iter().collect::<Vec<_>>();
 
-        let log_topics = topics
+        // Parses and obtains the topic string and its index.
+        let mut log_topic_ids = topics
             .into_iter()
             .map(|t| {
-                // The topic is separated by a `-` and the first part is the log topic id
                 let split = t.id.split('-').collect::<Vec<_>>();
 
-                // Get the log topic id
-                let log_topic_id = split[0];
+                // Ensure there are enough parts
+                if split.len() < 2 {
+                    return Err(eyre!("Unexpected topic format. Expected '<id>-<index>'."));
+                }
 
-                // Return the log topic
-                log_topic_id.parse::<ethers_main::types::H256>().unwrap()
+                // Parse the index
+                let index =
+                    split[1].parse::<usize>().map_err(|_| eyre!("Invalid number in topic id"))?;
+
+                // Parse log_topic_id
+                let log_topic_id = split[0]
+                    .parse::<ethers_main::types::H256>()
+                    .map_err(|_| eyre!("Error while parsing log topic"))?;
+
+                Ok((index, log_topic_id))
             })
+            .collect::<Result<Vec<(usize, ethers_main::types::H256)>>>()?;
+
+        // Sorts topics by their indexes.
+        log_topic_ids.sort_by_key(|(index, _)| *index);
+
+        // If only the sorted log_topic_ids are needed, map again to remove the index.
+        let log_topics = log_topic_ids
+            .into_iter()
+            .map(|(_, log_topic_id)| log_topic_id)
             .collect::<Vec<ethers_main::types::H256>>();
 
         let log = ethers_main::types::Log {
