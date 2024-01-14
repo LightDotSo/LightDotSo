0b2466ae17584820270f9d6bfc2dd2997cabcb0c
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-13 21:01:58 -0800

    chore: update

--- crates/simulator/src/simulator.rs
+++ crates/simulator/src/simulator.rs
@@ -20,6 +20,7 @@ use crate::{
     evm::Evm,
     types::{SimulationRequest, SimulationResponse},
 };
+use ethers::providers::Middleware;
 use ethers_main::abi::Uint;
 use eyre::{eyre, Result};
 use lightdotso_contracts::provider::get_provider;
@@ -29,8 +30,10 @@ async fn run(
     request: SimulationRequest,
     commit: bool,
 ) -> Result<SimulationResponse> {
+    // Get the value
     let value = request.value.map(Uint::from);
 
+    // Run the transaction and get the result
     let result = if commit {
         evm.call_raw_committing(request.from, request.to, value, request.data, request.gas_limit)
             .await
@@ -41,6 +44,7 @@ async fn run(
             .map_err(|_err| eyre!("Failed to call transaction"))?
     };
 
+    // Return the result
     Ok(SimulationResponse {
         gas_used: result.gas_used,
         block_number: request.block_number.unwrap_or(0),
@@ -52,25 +56,52 @@ async fn run(
 }
 
 pub async fn simulate(transaction: SimulationRequest) -> Result<SimulationResponse> {
-    let fork_url = get_provider(transaction.chain_id).await?.url().to_string();
-    let mut evm =
-        Evm::new(None, fork_url, transaction.block_number, transaction.gas_limit, true).await;
+    // Get the provider
+    let provider = get_provider(transaction.chain_id).await?;
 
+    // Get the fork url
+    let fork_url = provider.url().to_string();
+
+    // If block number is not provided, use the latest block number
+    let latest_block_number = provider.get_block_number().await?;
+
+    // Get the block number
+    let block_number = transaction.block_number.unwrap_or(latest_block_number.as_u64());
+
+    // Construct the EVM
+    let mut evm = Evm::new(None, fork_url, Some(block_number), transaction.gas_limit, true).await;
+
+    // Run the transaction
     let response = run(&mut evm, transaction, false).await?;
 
+    // Return the response
     Ok(response)
 }
 
 pub async fn simulate_bundle(
     transactions: Vec<SimulationRequest>,
 ) -> Result<Vec<SimulationResponse>> {
+    // Get the first chain id and block number
     let first_chain_id = transactions[0].chain_id;
     let first_block_number = transactions[0].block_number;
 
-    let fork_url = get_provider(first_chain_id).await?.url().to_string();
-    let mut evm =
-        Evm::new(None, fork_url, first_block_number, transactions[0].gas_limit, true).await;
+    // Get the provider
+    let provider = get_provider(first_chain_id).await?;
 
+    // Get the fork url
+    let fork_url = provider.url().to_string();
+
+    // If block number is not provided, use the latest block number
+    let latest_block_number = provider.get_block_number().await?;
+
+    // Get the block number
+    let block_number = first_block_number.unwrap_or(latest_block_number.as_u64());
+
+    // Construct the EVM
+    let mut evm =
+        Evm::new(None, fork_url, Some(block_number), transactions[0].gas_limit, true).await;
+
+    // Run the transactions
     let mut response = Vec::with_capacity(transactions.len());
     for transaction in transactions {
         if transaction.chain_id != first_chain_id {
@@ -82,5 +113,6 @@ pub async fn simulate_bundle(
         response.push(run(&mut evm, transaction, true).await?);
     }
 
+    // Return the response
     Ok(response)
 }
