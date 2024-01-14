213613371de0c3d189ece54605c071709dab3e46
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-14 15:33:42 -0800

    chore: ini table demo src

--- packages/demo/src/api/token/index.ts
+++ packages/demo/src/api/token/index.ts
@@ -13,8 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { tokenListData } from "@lightdotso/demo";
+import { TokenData } from "@lightdotso/data";
+import jsonData from "./list.json";
 
-export const TokenTable = () => {
-  return <>{JSON.stringify(tokenListData)}</>;
-};
+export const tokenListData = jsonData as readonly TokenData[];
