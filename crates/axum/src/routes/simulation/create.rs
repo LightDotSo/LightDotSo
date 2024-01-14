5a3c3e54f87eccc7f9aa338d252b85c36ff8de6d
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-01-13 21:08:05 -0800

    fix: simulator ops (#1379)
    * feat: ini sim create params
    
    * chore: update
    
    * chore: update
    
    * chore: update

--- crates/axum/src/routes/simulation/create.rs
+++ crates/axum/src/routes/simulation/create.rs
@@ -13,27 +13,66 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use crate::{result::AppJsonResult, state::AppState};
+use crate::{
+    result::AppJsonResult, routes::interpretation::types::Interpretation, state::AppState,
+};
 use autometrics::autometrics;
 use axum::{extract::State, Json};
-use lightdotso_db::models::activity::CustomParams;
+use clap::Parser;
+use lightdotso_db::models::{
+    activity::CustomParams, interpretation::upsert_interpretation_with_actions,
+};
+use lightdotso_interpreter::config::InterpreterArgs;
 use lightdotso_kafka::{
     topics::activity::produce_activity_message, types::activity::ActivityMessage,
 };
 use lightdotso_prisma::{ActivityEntity, ActivityOperation};
+use lightdotso_simulator::types::SimulationRequest;
+use lightdotso_tracing::tracing::info;
 // use lightdotso_tracing::tracing::info;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
 // -----------------------------------------------------------------------------
-// Query
+// Params
 // -----------------------------------------------------------------------------
 
 #[derive(Serialize, Deserialize, ToSchema, Clone)]
 #[serde(rename_all = "snake_case")]
 pub(crate) struct SimulationCreateRequestParams {
-    /// The id of the simulation to update for.
-    id: String,
+    /// The block number of the simulation to update for.
+    /// If not provided, the latest block number will be used.
+    pub block_number: Option<u64>,
+    /// The chain id of the simulation to update for.
+    pub chain_id: u64,
+    /// The from address of the simulation to update for.
+    pub from: String,
+    /// The to address of the simulation to update for.
+    pub to: String,
+    /// The data of the simulation to update for.
+    pub data: Option<String>,
+    /// The value of the simulation to update for.
+    pub value: Option<u64>,
+}
+
+// -----------------------------------------------------------------------------
+// Try From
+// -----------------------------------------------------------------------------
+
+impl TryFrom<SimulationCreateRequestParams> for SimulationRequest {
+    type Error = eyre::Report;
+
+    fn try_from(params: SimulationCreateRequestParams) -> Result<Self, Self::Error> {
+        Ok(Self {
+            block_number: params.block_number,
+            chain_id: params.chain_id,
+            from: params.from.parse()?,
+            to: params.to.parse()?,
+            data: params.data.map(|data| data.parse()).transpose()?,
+            value: params.value,
+            gas_limit: u64::MAX,
+        })
+    }
 }
 
 // -----------------------------------------------------------------------------
@@ -54,21 +93,28 @@ pub(crate) struct SimulationCreateRequestParams {
 pub(crate) async fn v1_simulation_create_handler(
     State(state): State<AppState>,
     Json(params): Json<SimulationCreateRequestParams>,
-) -> AppJsonResult<i64> {
+) -> AppJsonResult<Interpretation> {
     // -------------------------------------------------------------------------
     // Parse
     // -------------------------------------------------------------------------
 
     // Get the simulation from the post body.
-    let simulation = params.id;
+    let simulation_request = SimulationRequest::try_from(params.clone())?;
 
     // -------------------------------------------------------------------------
     // DB
     // -------------------------------------------------------------------------
 
     // Create the simulation the database.
-    // let simulation = state.client.simulation().create(vec![], vec![]).exec().await?;
-    // info!(?simulation);
+    let args = InterpreterArgs::parse_from([""]);
+
+    // Run the simulation.
+    let res = args.run(simulation_request).await?;
+    info!("res: {:?}", res);
+
+    // Upsert the interpretation
+    let interpretation =
+        upsert_interpretation_with_actions(state.client.clone(), res.clone(), None, None).await?;
 
     // -------------------------------------------------------------------------
     // Kafka
@@ -80,7 +126,7 @@ pub(crate) async fn v1_simulation_create_handler(
         ActivityEntity::Simulation,
         &ActivityMessage {
             operation: ActivityOperation::Create,
-            log: serde_json::to_value(&simulation)?,
+            log: serde_json::to_value(&params)?,
             params: CustomParams {
                 // wallet_address: Some(wallet.address.clone()),
                 // invite_code_id: Some(invite_code.id.clone()),
@@ -90,5 +136,12 @@ pub(crate) async fn v1_simulation_create_handler(
     )
     .await?;
 
-    Ok(Json::from(1))
+    // -------------------------------------------------------------------------
+    // Return
+    // -------------------------------------------------------------------------
+
+    // Change the interpretation to the format that the API expects.
+    let interpretation: Interpretation = interpretation.into();
+
+    Ok(Json::from(interpretation))
 }
