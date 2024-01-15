9b74316136229e4c8a4576763b11dc0beb05e308
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-14 23:35:05 -0800

    chore: update

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,11 +5,6 @@ import type PlaywrightTestConfig from "@playwright/test";
 const baseUrl = process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3000";
 console.warn(`Using base URL "${baseUrl}"`);
 
-// From: https://github.com/calcom/cal.com/blob/50d2dad62c5cc93b9ccfad6b9b0836062fd5f465/playwright.config.ts#L26
-const IS_AUTH_TEST = process.argv.some(a =>
-  a.startsWith("--project=@lightdotso/auth"),
-);
-
 const webServer: PlaywrightTestConfig["webServer"] = [
   {
     command: "pnpm turbo run dev --filter @lightdotso/app",
@@ -19,27 +14,15 @@ const webServer: PlaywrightTestConfig["webServer"] = [
   },
 ];
 
-if (IS_AUTH_TEST) {
-  webServer.push({
-    command: "pnpm turbo run dev --filter @lightdotso/auth",
-    port: 3000,
-    timeout: 300_000,
-    reuseExistingServer: !process.env.CI,
-  });
-}
-
 const opts = {
   headless: !!process.env.CI || !!process.env.PLAYWRIGHT_HEADLESS,
   collectCoverage: !!process.env.PLAYWRIGHT_HEADLESS,
 };
+
 const config: PlaywrightTestConfig = {
   retries: process.env.CI ? 3 : 0,
+  timeout: 300_000,
   projects: [
-    {
-      name: "@lightdotso/auth",
-      testDir: "./apps/auth/e2e",
-      testMatch: /.*\.spec\.tsx?/,
-    },
     {
       name: "@lightdotso/app",
       testDir: "./apps/app/e2e",
