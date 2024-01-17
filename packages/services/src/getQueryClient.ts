d8c4e740e105edcd530fd3946c5169470df3d489
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-16 19:22:50 -0800

    chore: update

--- packages/services/src/getQueryClient.ts
+++ packages/services/src/getQueryClient.ts
@@ -14,7 +14,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { QueryClient } from "@tanstack/react-query";
-// @ts-expect-error
 import { cache } from "react";
 
 // -----------------------------------------------------------------------------
