ed000a854f4166cb51e6bd10d6f5500c01d6cea5
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-17 15:46:17 -0800

    chore: ini queue

---
+++ packages/table/src/transaction/actions/transaction-table-row-actions.tsx
@@ -0,0 +1,73 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"use client";
+
+import { createQueueInterpretation } from "@lightdotso/client";
+import type { TransactionData } from "@lightdotso/data";
+import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@lightdotso/ui";
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import type { Row } from "@tanstack/react-table";
+
+// -----------------------------------------------------------------------------
+// Props
+// -----------------------------------------------------------------------------
+
+interface TransactionTableRowActionsProps {
+  row: Row<TransactionData>;
+}
+
+// -----------------------------------------------------------------------------
+// Component
+// -----------------------------------------------------------------------------
+
+export function TransactionTableRowActions({
+  row,
+}: TransactionTableRowActionsProps) {
+  // ---------------------------------------------------------------------------
+  // Render
+  // ---------------------------------------------------------------------------
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex size-8 p-0 data-[state=open]:bg-background-stronger"
+        >
+          <DotsHorizontalIcon className="size-4" />
+          <span className="sr-only">Open menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[160px]">
+        <DropdownMenuItem
+          onClick={() =>
+            createQueueInterpretation({
+              params: { query: { transaction_hash: row.original.hash } },
+            })
+          }
+        >
+          Queue Interpretation
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- packages/table/src/transaction/transaction-columns.tsx
+++ packages/table/src/transaction/transaction-columns.tsx
@@ -18,6 +18,7 @@
 import type { TransactionData } from "@lightdotso/data";
 import { DataTableColumnHeader } from "@lightdotso/ui";
 import type { ColumnDef } from "@tanstack/react-table";
+import { TransactionTableRowActions } from "./actions/transaction-table-row-actions";
 import { TransactionCardChain } from "./card/transaction-card-chain";
 import { TransactionCardHash } from "./card/transaction-card-hash";
 import { TransactionCardTimestamp } from "./card/transaction-card-timestamp";
@@ -62,5 +63,9 @@ export const transactionColumns: ColumnDef<TransactionData>[] = [
     },
     enableSorting: true,
     enableHiding: true,
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <TransactionTableRowActions row={row} />,
   },
 ];
