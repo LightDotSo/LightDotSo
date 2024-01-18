d728ed4dd938730ba19fd098ee372ccc4d6cd735
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-18 02:34:06 -0800

    chore: update

--- packages/state/tsup.config.ts
+++ packages/state/tsup.config.ts
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === "production";
 export default defineConfig({
   clean: true,
   dts: false,
-  entry: ["src/index.ts"],
+  entry: ["src/index.tsx"],
   format: ["cjs", "esm"],
   minify: isProduction,
   sourcemap: true,
