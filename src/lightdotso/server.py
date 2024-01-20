2322d17d912b09edd5bce4bf10796c59b509de52
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-01-19 18:47:44 -0800

    feat: ini smith server (#1419)

---
+++ src/lightdotso/server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# Copyright (C) 2023 Light, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatAnyscale
+from langserve import add_routes
+from langsmith import Client
+from dotenv import load_dotenv
+
+# Load .env variables
+load_dotenv()
+
+client = Client()
+
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="A simple api server using Langchain's Runnable interfaces",
+)
+
+add_routes(
+    app,
+    ChatAnyscale(),
+    path="/anyscale",
+)
+
+model = ChatAnyscale()
+prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
+add_routes(
+    app,
+    prompt | model,
+    path="/joke",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
