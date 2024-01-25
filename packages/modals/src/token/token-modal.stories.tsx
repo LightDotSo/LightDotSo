addfa376fbd630dbf40116bbd8e548df72226083
shunkakinoki <shunkakinoki@users.noreply.github.com>
2024-01-25 15:44:29 +0000

    chore: auto fix commit

--- packages/modals/src/token/token-modal.stories.tsx
+++ packages/modals/src/token/token-modal.stories.tsx
@@ -13,10 +13,10 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import type { Meta, StoryObj } from "@storybook/react";
-import { TokenModal } from "./token-modal";
 import { useModals } from "@lightdotso/stores";
+import type { Meta, StoryObj } from "@storybook/react";
 import { useEffect } from "react";
+import { TokenModal } from "./token-modal";
 
 // -----------------------------------------------------------------------------
 // Meta
