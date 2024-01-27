501a41b6ecb2b00922c6a182efbea5d25bb775dd
shunkakinoki <shunkakinoki@users.noreply.github.com>
2024-01-27 04:55:58 +0000

    chore: auto fix commit

--- packages/templates/src/footer-button/footer-button.tsx
+++ packages/templates/src/footer-button/footer-button.tsx
@@ -15,8 +15,8 @@
 
 "use client";
 
-import { cn } from "@lightdotso/utils";
 import { Button, ButtonIcon } from "@lightdotso/ui";
+import { cn } from "@lightdotso/utils";
 import { XIcon } from "lucide-react";
 import Link from "next/link";
 import type { FC } from "react";
