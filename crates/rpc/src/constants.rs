b60b7aacb9037cf0938ed14f8b3fb59153bef74e
Shun Kakinoki <shunkakinoki@gmail.com>
2024-02-01 02:25:29 -0800

    chore: update

--- crates/rpc/src/constants.rs
+++ crates/rpc/src/constants.rs
@@ -73,6 +73,7 @@ lazy_static! {
 
         // Testnet
         m.insert(80001, "https://polygon-mumbai.g.alchemy.com/v2/".to_string());
+        m.insert(84532, "https://base-sepolia.g.alchemy.com/v2/".to_string());
         m.insert(421614, "https://arb-sepolia.g.alchemy.com/v2/".to_string());
         m.insert(11155111, "https://eth-sepolia.g.alchemy.com/v2/".to_string());
         m.insert(11155420, "https://opt-sepolia.g.alchemy.com/v2/".to_string());
@@ -290,6 +291,33 @@ lazy_static! {
         m.insert(10200, "https://api.pimlico.io/v1/chiado-testnet/rpc".to_string());
         m.insert(80001, "https://api.pimlico.io/v1/mumbai/rpc".to_string());
         m.insert(11155111, "https://api.pimlico.io/v1/sepolia/rpc".to_string());
+
+        m
+    };
+}
+
+// The silius rpc urls
+lazy_static! {
+    pub static ref SILIUS_RPC_URLS: HashMap<u64, String> = {
+        let mut m = HashMap::new();
+
+        // Mainnet
+        m.insert(1, "https://rpc.silius.xyz/api/v1/chain/ethereum-mainnet".to_string());
+        m.insert(10, "https://rpc.silius.xyz/api/v1/chain/optimism-mainnet".to_string());
+        m.insert(56, "https://rpc.silius.xyz/api/v1/chain/bsc-mainnet".to_string());
+        m.insert(137, "https://rpc.silius.xyz/api/v1/chain/polygon-mainnet".to_string());
+        m.insert(8453, "https://rpc.silius.xyz/api/v1/chain/base-mainnet".to_string());
+        m.insert(42161, "https://rpc.silius.xyz/api/v1/chain/arbitrum-mainnet".to_string());
+        m.insert(43114, "https://rpc.silius.xyz/api/v1/chain/avalanche-mainnet".to_string());
+        m.insert(59144, "https://rpc.silius.xyz/api/v1/chain/linea-mainnet".to_string());
+
+
+        // Testnet
+        m.insert(80001, "https://rpc.silius.xyz/api/v1/chain/polygon-mumbai".to_string());
+        m.insert(84532, "https://rpc.silius.xyz/api/v1/chain/base-sepolia".to_string());
+        m.insert(421614, "https://rpc.silius.xyz/api/v1/chain/arbitrum-sepolia".to_string());
+        m.insert(11155111, "https://rpc.silius.xyz/api/v1/chain/ethereum-sepolia".to_string());
+        m.insert(11155420, "https://rpc.silius.xyz/api/v1/chain/optimism-sepolia".to_string());
 
         m
     };
