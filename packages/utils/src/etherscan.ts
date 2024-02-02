6dd0e57d2c44716604a710a8e8820d91e448e380
shunkakinoki <shunkakinoki@users.noreply.github.com>
2024-02-02 16:03:11 +0000

    chore: auto fix commit

--- packages/utils/src/etherscan.ts
+++ packages/utils/src/etherscan.ts
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Chain } from "viem";
+import type { Chain } from "viem";
 
 export const getEtherscanUrl = (chain: Chain) => {
   return (
