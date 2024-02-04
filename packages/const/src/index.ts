d2784a0b05ca73869e2604aeeb209f649c98f881
Shun Kakinoki <shunkakinoki@gmail.com>
2024-02-04 08:40:59 -0800

    chore: update const

--- packages/const/src/index.ts
+++ packages/const/src/index.ts
@@ -15,9 +15,11 @@
 
 export { Action, ACTION_LABELS } from "./actions";
 export {
+  BASE_API_ADMIN_URL,
   BASE_API_AUTHENTICATED_URL,
   BASE_API_URL,
   BASE_LOCAL_API_ADMIN_URL,
+  BASE_LOCAL_API_AUTHENTICATED_URL,
   BASE_LOCAL_API_URL,
   BASE_RPC_URL,
 } from "./api_urls";
