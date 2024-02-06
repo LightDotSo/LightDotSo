f06090a67a8e115b01b0cb47cbaddfb70b8e248b
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-02-06 15:01:51 -0800

    chore: ini tx op (#1581)
    * chore: update integration testing
    
    * chore: update tx
    
    * chore: update

---
+++ .changeset/dry-turtles-rhyme.md
@@ -0,0 +1,5 @@
+---
+"@lightdotso/crates": patch
+---
+
+Fix the relation set issues alltogether
--- crates/db/tests/it/transaction.rs
+++ crates/db/tests/it/transaction.rs
@@ -13,9 +13,15 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use ethers::providers::Middleware;
+use ethers_main::types::{H160, H256};
 use eyre::Result;
-use lightdotso_db::{db::create_test_client, models::transaction::get_transaction_with_logs};
-use std::sync::Arc;
+use lightdotso_contracts::provider::get_provider;
+use lightdotso_db::{
+    db::create_test_client,
+    models::transaction::{get_transaction_with_logs, upsert_transaction_with_log_receipt},
+};
+use std::{str::FromStr, sync::Arc};
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_integration_get_transaction_with_logs() -> Result<()> {
@@ -34,6 +40,52 @@ async fn test_integration_get_transaction_with_logs() -> Result<()> {
 
     // Print the transaction.
     println!("{:?}", tx);
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_integration_upsert_transaction_with_log_receipt() -> Result<()> {
+    // Load the environment variables.
+    let _ = dotenvy::dotenv();
+
+    // Create a database client.
+    let db = create_test_client().await?;
+
+    // Get the provider.
+    let provider = get_provider(1).await?;
+
+    // Set the tx hash.
+    let tx_hash =
+        H256::from_str("0xf8af7ce87505ad4611ec3beaf3f0f70ca4c73dff544347fac5fcf198b4e73ed0")?;
+
+    // Get a transaction with logs.
+    let tx = provider.get_transaction(tx_hash).await?;
+
+    // Get the transaction with logs.
+    let tx_with_receipt = provider.get_transaction_receipt(tx_hash).await?;
+
+    // Get the block number.
+    let block = provider.get_block(18917135).await?;
+
+    // Wallet address.
+    let wallet_address: H160 = "0xFbd80Fe5cE1ECe895845Fd131bd621e2B6A1345F".parse()?;
+
+    // Upsert the transaction with log receipt.
+    let res = upsert_transaction_with_log_receipt(
+        db.into(),
+        wallet_address,
+        tx.clone().unwrap(),
+        tx_with_receipt.clone().unwrap().logs,
+        tx_with_receipt.clone().unwrap(),
+        1,
+        block.clone().unwrap().timestamp,
+        None,
+    )
+    .await?;
+
+    // Print the result.
+    println!("{:?}", res);
 
     Ok(())
 }
