4e5c5212c43db171d1c90e124ffa54eee8654a87
Shun Kakinoki <shunkakinoki@gmail.com>
2024-02-07 21:10:34 -0800

    chore: update keys

--- crates/notifier/src/key.rs
+++ crates/notifier/src/key.rs
@@ -16,53 +16,23 @@
 use lazy_static::lazy_static;
 use lightdotso_prisma::{ActivityEntity, ActivityOperation};
 use serde_json::Value;
+use strum::IntoEnumIterator;
+use strum_macros::{Display, EnumIter, EnumString, IntoStaticStr};
 
-// The all supported platforms for the notification
-lazy_static! {
-    pub static ref PLATFORM_ANDROID: String = "android".to_string();
-    pub static ref PLATFORM_IOS: String = "ios".to_string();
-    pub static ref PLATFORM_WEB: String = "web".to_string();
+#[derive(Clone, Debug, EnumString, EnumIter, IntoStaticStr, Display)]
+pub enum Platform {
+    #[strum(serialize = "android")]
+    Android,
+    #[strum(serialize = "ios")]
+    IOS,
+    #[strum(serialize = "web")]
+    Web,
 }
 
-// The array of all supported platforms
-lazy_static! {
-    pub static ref PLATFORMS: Vec<String> =
-        vec![PLATFORM_ANDROID.to_string(), PLATFORM_IOS.to_string(), PLATFORM_WEB.to_string()];
-}
-
-// The user operation create topic
-lazy_static! {
-    pub static ref USER_OPERATION_CREATED: String = "user-operation-created".to_string();
-}
-
-// The user operation execute topic
-lazy_static! {
-    pub static ref USER_OPERATION_EXECUTED: String = "user-operation-executed".to_string();
-}
-
-// The transaction w/ user operation topic
-lazy_static! {
-    pub static ref TRANSACTION_WITH_USER_OPERATIOND_EXECUTE: String =
-        "transaction-with-user-operation-executed".to_string();
-}
-
-// The invite code accepted topic
-lazy_static! {
-    pub static ref INVITE_CODE_ACCEPTED: String = "invite-code-accepted".to_string();
-}
-
-// The array of all user only operations
-lazy_static! {
-    pub static ref USER_ONLY_OPERATIONS: Vec<String> = vec![INVITE_CODE_ACCEPTED.to_string(),];
-}
-
-// Utility function to check if the activity is user only
-pub fn is_user_only_activity(entity: &ActivityEntity, operation: &ActivityOperation) -> bool {
-    matches!(
-        (entity, operation),
-        (ActivityEntity::InviteCode, ActivityOperation::Update) |
-            (ActivityEntity::User, ActivityOperation::Create)
-    )
+#[derive(Clone, Debug, EnumString, EnumIter, IntoStaticStr, Display)]
+pub enum UserOnlyOperation {
+    #[strum(serialize = "invite-code-accepted")]
+    InviteCodeAccepted,
 }
 
 // Utility function to match the activity entity/operation combination
@@ -70,12 +40,12 @@ pub fn match_user_only_notification_with_activity(
     entity: &ActivityEntity,
     operation: &ActivityOperation,
     log: &Value,
-) -> Option<String> {
+) -> Option<UserOnlyOperation> {
     match (entity, operation) {
         (ActivityEntity::InviteCode, ActivityOperation::Update) => {
             // Check if the key of log is status is `USED`
             if log["status"].as_str() == Some("USED") {
-                Some(INVITE_CODE_ACCEPTED.to_string())
+                Some(UserOnlyOperation::InviteCodeAccepted)
             } else {
                 None
             }
@@ -84,43 +54,126 @@ pub fn match_user_only_notification_with_activity(
     }
 }
 
-// The array of all wallet only operations
-lazy_static! {
-    pub static ref WALLET_ONLY_OPERATIONS: Vec<String> = vec![
-        USER_OPERATION_CREATED.to_string(),
-        USER_OPERATION_EXECUTED.to_string(),
-        TRANSACTION_WITH_USER_OPERATIOND_EXECUTE.to_string()
-    ];
+#[derive(Clone, Debug, EnumString, EnumIter, IntoStaticStr, Display)]
+pub enum WalletOnlyOperation {
+    #[strum(serialize = "user-operation-created")]
+    UserOperationCreated,
+    #[strum(serialize = "user-operation-executed")]
+    UserOperationExecuted,
+    #[strum(serialize = "transaction-with-user-operation-executed")]
+    TransactionWithUserOperationExecuted,
 }
 
-// The array of all supported operations
-lazy_static! {
-    pub static ref OPERATIONS: Vec<String> = {
-        let mut vec = Vec::new();
-        for user_only_operation in USER_ONLY_OPERATIONS.iter() {
-            vec.push(user_only_operation.to_string());
+// Utility function to match the activity entity/operation combination
+pub fn match_wallet_only_notification_with_activity(
+    entity: &ActivityEntity,
+    operation: &ActivityOperation,
+    log: &Value,
+) -> Option<WalletOnlyOperation> {
+    match (entity, operation) {
+        (ActivityEntity::UserOperation, ActivityOperation::Create) => {
+            Some(WalletOnlyOperation::UserOperationCreated)
         }
-        for wallet_only_operation in WALLET_ONLY_OPERATIONS.iter() {
-            vec.push(wallet_only_operation.to_string());
+        (ActivityEntity::UserOperation, ActivityOperation::Update) => {
+            Some(WalletOnlyOperation::UserOperationExecuted)
         }
-        vec
-    };
-}
-
-// Utility function to format the combination of platform and operation
-fn format_combination(platform: &String, operation: &String) -> String {
-    format!("{}-{}", platform, operation)
-}
-
-// The full mapping of notification keys
-lazy_static! {
-    pub static ref NOTIFICATION_MAPPING: Vec<String> = {
-        let mut vec = Vec::new();
-        for platform in PLATFORMS.iter() {
-            for operation in OPERATIONS.iter() {
-                vec.push(format_combination(platform, operation));
+        (ActivityEntity::Transaction, ActivityOperation::Update) => {
+            if log["status"].as_str() == Some("EXECUTED") ||
+                log["status"].as_str() == Some("REVERTED")
+            {
+                Some(WalletOnlyOperation::TransactionWithUserOperationExecuted)
+            } else {
+                None
             }
         }
-        vec
+        _ => None,
+    }
+}
+
+#[derive(Clone, Debug, Display)]
+pub enum Operation {
+    UserOnly(UserOnlyOperation),
+    WalletOnly(WalletOnlyOperation),
+}
+
+lazy_static! {
+    pub static ref OPERATIONS: Vec<Operation> = {
+        let mut operations = Vec::new();
+
+        for operation in UserOnlyOperation::iter() {
+            operations.push(Operation::UserOnly(operation));
+        }
+
+        for operation in WalletOnlyOperation::iter() {
+            operations.push(Operation::WalletOnly(operation));
+        }
+
+        operations
     };
+}
+
+// For each operation, we define a notification type which is all combinations of operation and
+// platform
+
+#[derive(Clone, Debug)]
+pub enum Notification {
+    UserOnly(Platform, UserOnlyOperation),
+    WalletOnly(Platform, WalletOnlyOperation),
+}
+
+impl std::fmt::Display for Notification {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Notification::UserOnly(platform, operation) => {
+                write!(f, "{:?}-{:?}", platform, operation)
+            }
+            Notification::WalletOnly(platform, operation) => {
+                write!(f, "{:?}-{:?}", platform, operation)
+            }
+        }
+    }
+}
+
+lazy_static! {
+    pub static ref NOTIFICATION: Vec<Notification> = {
+        let mut notification_types = Vec::new();
+
+        for operation in UserOnlyOperation::iter() {
+            for platform in Platform::iter() {
+                notification_types.push(Notification::UserOnly(platform, operation.clone()));
+            }
+        }
+
+        for operation in WalletOnlyOperation::iter() {
+            for platform in Platform::iter() {
+                notification_types.push(Notification::WalletOnly(platform, operation.clone()));
+            }
+        }
+
+        notification_types
+    };
+}
+
+lazy_static! {
+    pub static ref NOTIFICATION_KEYS: Vec<String> = {
+        let mut notification_keys = Vec::new();
+
+        for notification in NOTIFICATION.iter() {
+            notification_keys.push(notification.to_string());
+        }
+
+        notification_keys
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_notification_keys() {
+        for key in NOTIFICATION_KEYS.iter() {
+            println!("{}", key);
+        }
+    }
 }
