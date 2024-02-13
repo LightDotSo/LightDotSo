aea7183c4d5acde6385c5fcaa4563993632d1815
Shun Kakinoki <shunkakinoki@gmail.com>
2024-02-13 10:56:08 -0800

    feat: ini address form

---
+++ packages/forms/src/address-form.stories.tsx
@@ -0,0 +1,54 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import type { Meta, StoryObj } from "@storybook/react";
+import { useForm } from "react-hook-form";
+import { AddressForm } from "./address-form";
+import { Form } from "@lightdotso/ui";
+
+// -----------------------------------------------------------------------------
+// Meta
+// -----------------------------------------------------------------------------
+
+const meta: Meta<typeof AddressForm> = {
+  title: "form/AddressForm",
+  component: AddressForm,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+export default meta;
+
+// -----------------------------------------------------------------------------
+// Types
+// -----------------------------------------------------------------------------
+
+type Story = StoryObj<typeof AddressForm>;
+
+// -----------------------------------------------------------------------------
+// Story
+// -----------------------------------------------------------------------------
+
+export const Base: Story = {
+  render: args => {
+    const methods = useForm();
+
+    return (
+      <Form {...methods}>
+        <AddressForm name="address" />
+      </Form>
+    );
+  },
+  args: {},
+};
---
+++ packages/forms/src/address-form.tsx
@@ -0,0 +1,91 @@
+// Copyright (C) 2023 Light, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"use client";
+
+import { PlaceholderOrb } from "@lightdotso/elements";
+import { Avatar, FormField, FormMessage, Input, Label } from "@lightdotso/ui";
+// import { useEnsName } from "@lightdotso/wagmi";
+import { useFormContext } from "react-hook-form";
+import type { FC } from "react";
+import { isAddress, type Address } from "viem";
+
+// -----------------------------------------------------------------------------
+// Props
+// -----------------------------------------------------------------------------
+
+type AddressFormProps = { name: string; className?: string };
+
+// -----------------------------------------------------------------------------
+// Component
+// -----------------------------------------------------------------------------
+
+export const AddressForm: FC<AddressFormProps> = ({ name }) => {
+  // ---------------------------------------------------------------------------
+  // Wagmi
+  // ---------------------------------------------------------------------------
+
+  // const { data: ens } = useEnsName({ address: address, chainId: 1 });
+
+  // ---------------------------------------------------------------------------
+  // Form
+  // ---------------------------------------------------------------------------
+
+  const methods = useFormContext();
+
+  // ---------------------------------------------------------------------------
+  // Render
+  // ---------------------------------------------------------------------------
+
+  if (!methods) {
+    return null;
+  }
+
+  return (
+    <FormField
+      name={name}
+      control={methods.control}
+      render={({ field }) => (
+        <div className="col-span-6 space-y-2">
+          <Label htmlFor="address">Address or ENS</Label>
+          <div className="flex items-center space-x-3">
+            <div className="relative inline-block w-full">
+              <Input
+                id="address"
+                className="pl-12"
+                {...field}
+                placeholder="Your address or ENS name"
+              />
+              <div className="absolute inset-y-0 left-3 flex items-center">
+                <Avatar className="size-6">
+                  {/* If the address is valid, try resolving an ens Avatar */}
+                  <PlaceholderOrb
+                    address={
+                      // If the address is a valid address
+                      field?.value && isAddress(field.value)
+                        ? field?.value
+                        : "0x4fd9D0eE6D6564E80A9Ee00c0163fC952d0A45Ed"
+                    }
+                  />
+                </Avatar>
+              </div>
+            </div>
+          </div>
+          <FormMessage />
+        </div>
+      )}
+    />
+  );
+};
