2cbdfc42c2844be0336ba3fe2b6878d68aa057a7
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-02-12 21:23:45 -0800

    chore: ini transfers query (#1643)

--- packages/nuqs/src/useUserOperationsQueryState.ts
+++ packages/nuqs/src/useUserOperationsQueryState.ts
@@ -129,7 +129,7 @@ export const useUserOperationsQueryState = (
   return useQueryState(
     "userOperations",
     userOperationsParser.withDefault(defaultUserOperations ?? []).withOptions({
-      throttleMs: 700,
+      throttleMs: 3000,
     }),
   );
 };
