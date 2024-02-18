e73f0b0713dde24067fc8c36b625a6c1d4e29819
Shun Kakinoki <shunkakinoki@gmail.com>
2024-02-17 20:27:11 -0800

    chore: ini base wrapper refactor

--- packages/ui/src/wrappers/base/base-wrapper.ts
+++ packages/ui/src/wrappers/base/base-wrapper.ts
@@ -15,4 +15,4 @@
 import { cn } from "@lightdotso/utils";
 
 export const baseWidthWrapper = cn("px-4 md:px-6 lg:px-8");
-export const baseHeightWrapper = cn("mb-8 mt-4 md:mb-10 md:mt-8 lg:mb-12");
+export const baseHeightWrapper = cn("my-8 md:my-10 lg:my-12");
