ba0cc2b3d3703c1686a29412cffd52cb34e34284
Shun Kakinoki <shunkakinoki@gmail.com>
2024-02-19 13:16:14 -0800

    chore: update slot

--- crates/axum/src/routes/user_operation/get.rs
+++ crates/axum/src/routes/user_operation/get.rs
@@ -76,6 +76,8 @@ pub(crate) async fn v1_user_operation_get_handler(
         .client
         .user_operation()
         .find_unique(user_operation::hash::equals(query.user_operation_hash))
+        .with(user_operation::paymaster::fetch())
+        .with(user_operation::paymaster_operation::fetch())
         .with(user_operation::transaction::fetch())
         .with(user_operation::signatures::fetch(vec![signature::user_operation_hash::equals(
             user_operation_hash,
