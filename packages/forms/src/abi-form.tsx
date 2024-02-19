587ff048aa388f70304d2402fa6efcad9cb6f228
Shun Kakinoki <shunkakinoki@gmail.com>
2024-02-18 23:33:09 -0800

    chore: update

--- packages/forms/src/abi-form.tsx
+++ packages/forms/src/abi-form.tsx
@@ -14,11 +14,20 @@
 
 "use client";
 
-import { getInviteCode as getClientInviteCode } from "@lightdotso/client";
-import type { RefinementCallback } from "@lightdotso/hooks";
-import { useRefinement } from "@lightdotso/hooks";
-import { newFormSchema } from "@lightdotso/schemas";
-import { Form } from "@lightdotso/ui";
+// import { getAbi as getClientAbi } from "@lightdotso/client";
+// import type { RefinementCallback } from "@lightdotso/hooks";
+// import { useRefinement } from "@lightdotso/hooks";
+import { abi } from "@lightdotso/schemas";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Textarea,
+} from "@lightdotso/ui";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   useEffect,
@@ -27,16 +36,16 @@ import {
   useCallback,
 } from "react";
 import { useForm, useFormContext } from "react-hook-form";
-import type { z } from "zod";
-import { AddressForm } from "./address-form";
+// import type { z } from "zod";
+// import { AddressForm } from "./address-form";
 
 // -----------------------------------------------------------------------------
 // Types
 // -----------------------------------------------------------------------------
 
-const inviteCodeSchema = newFormSchema.pick({ inviteCode: true });
+const abiFormSchema = abi;
 
-type InviteCodeValues = z.infer<typeof inviteCodeSchema>;
+// type AbiFormValues = z.infer<typeof abiFormSchema>;
 
 // -----------------------------------------------------------------------------
 // Props
@@ -57,48 +66,49 @@ export const AbiForm: FC<AbiFormProps> = ({ name }) => {
 
   const parentMethods = useFormContext();
 
-  const getInviteCode: RefinementCallback<InviteCodeValues> = async ({
-    inviteCode,
-  }: {
-    inviteCode: string;
-  }) => {
-    const res = await getClientInviteCode({
-      params: {
-        query: {
-          code: inviteCode,
-        },
-      },
-    });
+  // const getAbi: RefinementCallback<AbiValues> = async ({
+  //   abi,
+  // }: {
+  //   abi: string;
+  // }) => {
+  //   const res = await getClientAbi({
+  //     params: {
+  //       query: {
+  //         code: abi,
+  //       },
+  //     },
+  //   });
 
-    return res.match(
-      data => data.status === "ACTIVE",
-      () => false,
-    );
-  };
+  //   return res.match(
+  //     data => data.status === "ACTIVE",
+  //     () => false,
+  //   );
+  // };
 
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const validInviteCode = useRefinement(getInviteCode, {
-    debounce: 300,
-  });
+  // // eslint-disable-next-line react-hooks/rules-of-hooks
+  // const validAbi = useRefinement(getAbi, {
+  //   debounce: 300,
+  // });
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const form = useForm({
     mode: "all",
     reValidateMode: "onBlur",
-    resolver: zodResolver(
-      newFormSchema.pick({ inviteCode: true }).refine(
-        ({ inviteCode }) => {
-          if (inviteCode.length < 1) {
-            return true;
-          }
-          return validInviteCode({ inviteCode: inviteCode });
-        },
-        {
-          path: ["inviteCode"],
-          message: "Invite code is not valid",
-        },
-      ),
-    ),
+    resolver: zodResolver(abiFormSchema),
+    // resolver: zodResolver(
+    //   newFormSchema.pick({ abi: true }).refine(
+    //     ({ abi }) => {
+    //       if (abi.length < 1) {
+    //         return true;
+    //       }
+    //       return validAbi({ abi: abi });
+    //     },
+    //     {
+    //       path: ["abi"],
+    //       message: "Invite code is not valid",
+    //     },
+    //   ),
+    // ),
   });
 
   // ---------------------------------------------------------------------------
@@ -148,7 +158,28 @@ export const AbiForm: FC<AbiFormProps> = ({ name }) => {
 
   return (
     <Form {...form}>
-      <AddressForm name="address" onMouseLeave={validInviteCode.invalidate} />
+      <FormField
+        control={form.control}
+        name="abi"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>ABI</FormLabel>
+            <FormControl>
+              <Textarea
+                placeholder="Paste your ABI here"
+                className="resize-none"
+                {...field}
+              />
+            </FormControl>
+            <FormDescription>
+              You can directly paste the ABI of the contract you want to
+              interact with.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      {/* <AddressForm name="address" onMouseLeave={validAbi.invalidate} /> */}
     </Form>
   );
 };
