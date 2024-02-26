48dcde90ce719e1236fa49ba3e9b88a21ed8253b
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-02-25 17:48:22 -0700

    feat: ini app clients (#1721)
    * feat: ini client configs
    
    * chore: update
    
    * chore: update
    
    * chore: update

--- apps/inngest/src/inngest/walletRedisCron.ts
+++ apps/inngest/src/inngest/walletRedisCron.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { redis } from "@/client/redis";
+import { redis } from "@lightdotso/redis";
 import { inngest } from "@/inngest/client";
 
 // -----------------------------------------------------------------------------
