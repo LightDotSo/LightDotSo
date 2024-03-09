cd046ffd92d97cf47354ed99435d00d0291668e8
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-03-08 22:37:00 -0800

    fix: ini package addrs (#1763)
    * fix: ini package addrs
    
    * feat: ini package addrs
    
    * chore: update

---
+++ .changeset/wicked-dragons-obey.md
@@ -0,0 +1,32 @@
+---
+"@lightdotso/validators": patch
+"@lightdotso/templates": patch
+"@lightdotso/elements": patch
+"@lightdotso/sequence": patch
+"@lightdotso/schemas": patch
+"@lightdotso/modals": patch
+"@lightdotso/params": patch
+"@lightdotso/states": patch
+"@lightdotso/stores": patch
+"@lightdotso/tables": patch
+"@lightdotso/changelog": patch
+"@lightdotso/const": patch
+"@lightdotso/forms": patch
+"@lightdotso/hooks": patch
+"@lightdotso/query": patch
+"@lightdotso/utils": patch
+"@lightdotso/wagmi": patch
+"@lightdotso/explorer": patch
+"@lightdotso/nuqs": patch
+"@lightdotso/desktop": patch
+"@lightdotso/inngest": patch
+"@lightdotso/msw": patch
+"@lightdotso/assets": patch
+"@lightdotso/frames": patch
+"@lightdotso/ui": patch
+"@lightdotso/docs": patch
+"@lightdotso/home": patch
+"@lightdotso/app": patch
+---
+
+Add package addrs
--- packages/const/src/contract_addresses.ts
+++ packages/const/src/contract_addresses.ts
@@ -17,6 +17,7 @@ import type { Address } from "viem";
 /* eslint-disable no-unused-vars */
 export enum ContractAddress {
   V010_FACTORY = "v0.1.0 Factory",
+  V020_FACTORY = "v0.2.0 Factory",
   V060_ENTRYPOINT = "v0.6.0 Entrypoint",
 }
 
@@ -24,6 +25,7 @@ export const CONTRACT_ADDRESSES: {
   readonly [key in ContractAddress]: Address;
 } = {
   [ContractAddress.V010_FACTORY]: "0x0000000000756D3E6464f5efe7e413a0Af1C7474",
+  [ContractAddress.V020_FACTORY]: "0x00000000001269b052C004FFB71B47AB22C898B0",
   [ContractAddress.V060_ENTRYPOINT]:
     "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
 };
@@ -33,5 +35,7 @@ export const WALLET_FACTORY_ENTRYPOINT_MAPPING: {
   readonly [key in ContractAddress]: Address;
 } = {
   [ContractAddress.V010_FACTORY]:
+    CONTRACT_ADDRESSES[ContractAddress.V060_ENTRYPOINT],
+  [ContractAddress.V020_FACTORY]:
     CONTRACT_ADDRESSES[ContractAddress.V060_ENTRYPOINT],
 };
