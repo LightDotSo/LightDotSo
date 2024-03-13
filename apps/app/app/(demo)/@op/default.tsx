9dfdcdf697c058a4f03dba7bd3012d8ace15b6d3
Shun Kakinoki <shunkakinoki@gmail.com>
2024-03-13 08:32:41 -0700

    chore: update

--- apps/app/app/(demo)/@op/default.tsx
+++ apps/app/app/(demo)/@op/default.tsx
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import OriginalDefault from "@/app/(wallet)/@create/default";
+import OriginalDefault from "@/app/(wallet)/@op/default";
 
 // -----------------------------------------------------------------------------
 // Original Default
