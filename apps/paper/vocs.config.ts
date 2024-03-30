f650e2557f94fdf7612d485d7fa1a4696e31ee1b
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-03-30 10:34:59 -0400

    chore: ini draft (#1843)
    * chore: ini draft
    
    * chore: auto fix commit
    
    ---------
    
    Co-authored-by: shunkakinoki <shunkakinoki@users.noreply.github.com>

--- apps/paper/vocs.config.ts
+++ apps/paper/vocs.config.ts
@@ -4,6 +4,44 @@ export default defineConfig({
   title: "Lightpaper",
   sidebar: [
     {
+      text: "Introduction",
+      collapsed: false,
+      items: [
+        {
+          text: "What is Light",
+          link: "/introduction/what-is-light",
+        },
+        {
+          text: "Chain Abstraction",
+          link: "/introduction/chain-abstraction",
+        },
+        {
+          text: "Products",
+          link: "/introduction/products",
+        },
+      ],
+    },
+    {
+      text: "Concepts",
+      collapsed: false,
+      items: [
+        {
+          text: "Comparison",
+          link: "/concepts/comparison",
+        },
+      ],
+    },
+    {
+      text: "Goals",
+      collapsed: false,
+      items: [
+        {
+          text: "Endgame",
+          link: "/goals/endgame",
+        },
+      ],
+    },
+    {
       text: "Getting Started",
       link: "/getting-started",
     },
