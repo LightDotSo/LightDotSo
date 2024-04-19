9963d74c02bf24cb1a37b736e5e4515ee42a0933
Shun Kakinoki <shunkakinoki@gmail.com>
2024-04-19 16:21:11 -0700

    chore: update

--- apps/frames/app/layout.tsx
+++ apps/frames/app/layout.tsx
@@ -12,15 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// From: https://github.com/Zizzamia/a-frame-in-100-lines/blob/1e401d0c8ecc8d1b9ef5b31e92723eede9f06c51/app/layout.tsx
-// License: MIT
-
 import type { ReactNode } from "react";
-
-export const viewport = {
-  width: "device-width",
-  initialScale: 1.0,
-};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
