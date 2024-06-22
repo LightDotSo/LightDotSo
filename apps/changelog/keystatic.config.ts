d928b7d1787ea6e2da97ec7adabc5778e37667b2
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-06-22 13:00:51 +0900

    feat: ini changelog keystatic (#1985)

---
+++ apps/changelog/keystatic.config.ts
@@ -0,0 +1,23 @@
+import { config, fields, collection } from "@keystatic/core";
+
+export default config({
+  storage: {
+    kind: "cloud",
+    pathPrefix: "apps/changelog",
+  },
+  cloud: {
+    project: "lightdotso/changelog",
+  },
+  collections: {
+    posts: collection({
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        content: fields.markdoc({ label: "Content" }),
+      },
+    }),
+  },
+});
