6285095e8c62cc44c63eec20760b8a34ecbb9706
Shun Kakinoki <shunkakinoki@gmail.com>
2024-07-09 00:57:23 +0900

    feat: ini form state

--- packages/hooks/src/useUserOperationsSendState.ts
+++ packages/hooks/src/useUserOperationsSendState.ts
@@ -30,27 +30,36 @@ export const useUserOperationsSendState = () => {
   // Memoized Hooks
   // ---------------------------------------------------------------------------
 
-  // Check if the userOperation is loading
+  // Check if the userOperation is loading and there is only one status
+  const isUserOperationsSendLoadingSingle = useMemo(() => {
+    return (
+      userOperationSendStatus?.length === 1 &&
+      userOperationSendStatus[0] === "pending"
+    );
+  }, [userOperationSendStatus]);
+
+  // Check if the userOperations are all loading
   const isUserOperationsSendLoading = useMemo(() => {
     return userOperationSendStatus?.some(status => status === "pending");
   }, [userOperationSendStatus]);
 
-  // Check if the userOperation is successful
+  // Check if the userOperations are all successful
   const isUserOperationsSendSuccess = useMemo(() => {
     return userOperationSendStatus?.some(status => status === "success");
   }, [userOperationSendStatus]);
 
-  // Check if the userOperation is failed
+  // Check if the userOperations are all failed
   const isUserOperationsSendError = useMemo(() => {
     return userOperationSendStatus?.some(status => status === "error");
   }, [userOperationSendStatus]);
 
-  // Check if the userOperation is idle
+  // Check if the userOperations are all idle
   const isUserOperationsSendIdle = useMemo(() => {
     return userOperationSendStatus?.some(status => status === "idle");
   }, [userOperationSendStatus]);
 
   return {
+    isUserOperationsSendLoadingSingle: isUserOperationsSendLoadingSingle,
     isUserOperationsSendLoading: isUserOperationsSendLoading,
     isUserOperationsSendSuccess: isUserOperationsSendSuccess,
     isUserOperationsSendError: isUserOperationsSendError,
