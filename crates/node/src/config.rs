6f4b64150707460e266c575bee13d1c7f94acbb2
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-07-10 21:16:23 +0900

    chore: ini update clash (#2088)
    * chore: update
    
    * chore: update ncu
    
    * chore: update
    
    * chore: ini nap

---
+++ crates/node/src/config.rs
@@ -0,0 +1,35 @@
+// Copyright 2023-2024 LightDotSo.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::node::Node;
+use clap::Parser;
+use eyre::Result;
+use lightdotso_tracing::tracing::info;
+
+#[derive(Debug, Clone, Parser, Default)]
+pub struct NodeArgs {}
+
+impl NodeArgs {
+    pub async fn create(&self) -> Result<Node> {
+        // Add info
+        info!("NodeArgs run, starting...");
+
+        // Print the config
+        info!("Config: {:?}", self);
+
+        let node = Node::new(self).await;
+
+        Ok(node)
+    }
+}
