e8818d5aab22b2500990a86f1fe8cbdd24a070c7
Shun Kakinoki <shunkakinoki@gmail.com>
2024-07-10 21:13:30 +0900

    chore: ini nap

---
+++ crates/node/src/node.rs
@@ -0,0 +1,33 @@
+// Copyright 2023-2024 LightDotSo.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::config::NodeArgs;
+
+use lightdotso_tracing::tracing::info;
+
+#[derive(Clone)]
+pub struct Node {}
+
+impl Node {
+    pub async fn new(_args: &NodeArgs) -> Self {
+        info!("Node new, starting");
+
+        // Create the node
+        Self {}
+    }
+
+    pub async fn run(&self) {
+        info!("Node run, starting");
+    }
+}
