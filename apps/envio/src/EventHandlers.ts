20189cb84df69eca853306c9b5491f36058a4d0d
Shun Kakinoki <shunkakinoki@gmail.com>
2024-07-31 02:39:17 +0900

    chore: update

--- apps/envio/src/EventHandlers.ts
+++ apps/envio/src/EventHandlers.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { EntrypointContract } from "../generated/src/Handlers.gen";
+import { EntrypointContract } from "../generated/src/Handlers.res";
 
 EntrypointContract.UserOperationEvent.loader(({ event, context }) => {});
 
