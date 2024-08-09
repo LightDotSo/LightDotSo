115e479a78ee828f56ab33c1015bf33d91f9af3c
Shun Kakinoki <shunkakinoki@gmail.com>
2024-08-10 01:06:04 +0900

    chore: update

--- apps/ai/src/components/chat-panel.tsx
+++ apps/ai/src/components/chat-panel.tsx
@@ -99,7 +99,6 @@ export function ChatPanel({
         isAtBottom={isAtBottom}
         scrollToBottom={scrollToBottom}
       />
-
       <div className="mx-auto sm:max-w-2xl sm:px-4">
         <div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
           {messages.length === 0 &&
