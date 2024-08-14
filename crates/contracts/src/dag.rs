a65b3df7762f9ca57ba353347838cd03fbd29212
Shun Kakinoki <shunkakinoki@gmail.com>
2024-08-14 19:47:53 +0900

    chore: update

--- crates/contracts/src/dag.rs
+++ crates/contracts/src/dag.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(clippy::unwrap_used)]
+
 use abi::{decode, InvalidOutputType, ParamType, Token, Tokenizable, TokenizableItem};
 use ethers::{
     prelude::*,
