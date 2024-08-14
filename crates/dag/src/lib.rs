fe224b6e79cac83c57f7bea0d804455a8fb86aaf
Shun Kakinoki <shunkakinoki@gmail.com>
2024-08-14 19:50:31 +0900

    chore: update

--- crates/dag/src/lib.rs
+++ crates/dag/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(clippy::unwrap_used)]
+
 use dagrs::{Action, CommandAction, Dag, DagError, Parser, Task};
 use std::{
     collections::HashMap,
