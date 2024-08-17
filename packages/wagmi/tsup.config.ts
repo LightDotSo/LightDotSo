9e6c970226948ed47fd926811762f795002c6aac
Shun Kakinoki <shunkakinoki@gmail.com>
2024-08-18 08:16:20 +0900

    feat: ini dts

--- packages/wagmi/tsup.config.ts
+++ packages/wagmi/tsup.config.ts
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   clean: true,
-  dts: false,
+  dts: true,
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   minify: isProduction,
