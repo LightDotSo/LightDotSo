8e19e766dd9df10e1d6e04fb4e3788df319949f4
Shun Kakinoki <shunkakinoki@gmail.com>
2024-08-20 17:16:02 +0900

    feat: ini rclone assets

--- assets/sync.sh
+++ assets/sync.sh
@@ -14,4 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-rclone sync r2:assets ./ -P
+rclone sync r2:assets ./ -P --exclude exclude.txt --exclude .gitignore --exclude copy.sh --exclude sync.sh
