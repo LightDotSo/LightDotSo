4597c2239c4b1a828825b516138e7bebf02eae45
Shun Kakinoki <shunkakinoki@gmail.com>
2024-08-20 17:26:20 +0900

    chore: update

--- assets/meta.sh
+++ assets/meta.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+pnpm ardrive list-folder --parent-folder-id d63c3f8d-cfb6-4744-a977-e2ba63823da5 > meta/blog.json
 pnpm ardrive list-folder --parent-folder-id e64326f2-d457-4dd8-85cb-27e1b9f3969b > meta/root.json
 pnpm ardrive list-folder --parent-folder-id c10c6082-5512-4596-864c-93e4c66d896d > meta/paper.json
 pnpm ardrive list-folder --parent-folder-id dd2b4fc4-4075-4970-89a5-57d06b683ba6 > meta/proposals.json
