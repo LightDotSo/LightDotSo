fe1bc04fff4caab506c074e714c3d8c8f6f98cab
Shun Kakinoki <shunkakinoki@gmail.com>
2024-08-21 14:47:30 +0900

    chore: ini refactor home

--- apps/home/next.config.mjs
+++ apps/home/next.config.mjs
@@ -22,7 +22,8 @@ import packageJson from "./package.json" assert { type: "json" };
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/home",
+  basePath:
+    process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "/home" : undefined,
   env: {
     NEXT_PUBLIC_APP_VERSION: `@lightdotso/home@${packageJson.version}`,
   },
