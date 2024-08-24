5ac47d3f53389ca0bd61801a026ae25df883cd3a
Shun Kakinoki <shunkakinoki@gmail.com>
2024-08-25 04:31:35 +0900

    chore: update

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -38,8 +38,8 @@ export {
 } from "./components/avatar";
 export { Badge, badgeVariants } from "./components/badge";
 export { BadgeIcon } from "./components/badge-icon";
-export { Button, buttonVariants } from "./components/button/button";
-export type { ButtonProps } from "./components/button/button";
+export { Button, buttonVariants } from "./components/button";
+export type { ButtonProps } from "./components/button";
 export { ButtonGroup } from "./components/button-group";
 export { ButtonIcon } from "./components/button-icon";
 export type { ButtonIconProps } from "./components/button-icon";
