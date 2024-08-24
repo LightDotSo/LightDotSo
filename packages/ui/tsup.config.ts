6daea2c203a091d10a4c75b7e13973147ca8027e
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-08-25 02:41:20 +0900

    fix: ini treeshaking (#2269)
    * fix: ini treeshaking
    
    * chore: update

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -5,6 +5,8 @@ const isProduction = process.env.NODE_ENV === "production";
 export default defineConfig({
   clean: true,
   dts: true,
+  treeshake: true,
+  splitting: true,
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   minify: isProduction,
