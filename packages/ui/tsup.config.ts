543ddd636ed0a395626c35f7316b508e852de7b1
Shun Kakinoki <shunkakinoki@gmail.com>
2024-08-25 21:33:44 +0900

    chore: udpate

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   clean: true,
-  dts: false,
+  dts: true,
   treeshake: true,
   splitting: true,
   entry: ["src/index.ts"],
