b5ae7e952636cf2273b2260288cac11bc5be089c
Shun Kakinoki <shunkakinoki@gmail.com>
2024-08-27 09:57:10 +0900

    chore: update

--- apps/app/app/(wallet)/[address]/overview/(components)/overview/overview-list.tsx
+++ apps/app/app/(wallet)/[address]/overview/(components)/overview/overview-list.tsx
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+"use client";
+
 import { OverviewCard } from "@/app/(wallet)/[address]/overview/(components)/overview/overview-card";
 import { OVERVIEW_NAV_ITEMS } from "@/app/(wallet)/[address]/overview/(const)/nav-items";
 import { OverviewSubCategory, TITLES } from "@/const";
