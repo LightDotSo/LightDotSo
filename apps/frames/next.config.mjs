fbe24ab3eef304665a4a56987557c6a51f323aa6
Shun Kakinoki <shunkakinoki@gmail.com>
2024-08-28 16:08:12 +0900

    chore: update

--- apps/frames/next.config.mjs
+++ apps/frames/next.config.mjs
@@ -25,27 +25,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    instrumentationHook: true,
-    // From: https://github.com/vercel/next.js/issues/42641
-    outputFileTracingExcludes: {
-      "*": [
-        "./node_modules/@swc/core-linux-x64-gnu",
-        "./node_modules/@swc/core-linux-x64-musl",
-        "./node_modules/esbuild-linux-64/bin",
-        "./node_modules/webpack/lib",
-        "./node_modules/rollup",
-        "./node_modules/terser",
-      ],
-    },
-  },
-  outputFileTracing: true,
-  transpilePackages: ["@lightdotso/ui"],
-  webpack: (config) => {
-    config.externals.push("async_hooks", "pino-pretty", "lokijs", "encoding");
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-
-    return config;
+  outputFileTracingExcludes: {
+    "*": [
+      "./node_modules/@swc/core-linux-x64-gnu",
+      "./node_modules/@swc/core-linux-x64-musl",
+      "./node_modules/esbuild-linux-64/bin",
+      "./node_modules/webpack/lib",
+      "./node_modules/rollup",
+      "./node_modules/terser",
+    ],
   },
 };
 
