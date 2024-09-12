7f1ca5773d3f9afdf81c1b10d7df9046b45c28a8
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-09-13 07:26:01 +0900

    fix: refactor app (#2363)
    * feat: ini app open
    
    * fix: lintstaged
    
    * fix: update

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-  "*": ["pnpm run license:cmd add"],
-  "*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}": ["pnpm biome:cmd"],
+  "*": [
+    "pnpm run license:cmd add",
+    // "pnpm run cspell:cmd",
+  ],
+  "*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc,css,graphql}": [
+    "pnpm biome:cmd",
+  ],
   // "*.{md,mdx}": ["pnpm dprint fmt"],
   "*.rs": [
     "sh -c 'pnpm run clippy:cmd && echo \"\"'",
@@ -14,10 +19,9 @@ module.exports = {
   ],
   "*.toml": ["pnpm run taplo:cmd"],
   "package.json": [
-    "pnpm run sherif:lint",
+    "pnpm run sherif:cmd",
     "pnpm run npm-package-json:lint",
     "pnpm run sort-package-json:fix",
-    "pnpm run biome:cmd",
   ],
   "thunder-tests/**/*.json": ["./scripts/check_thunder_url.sh"],
 };
