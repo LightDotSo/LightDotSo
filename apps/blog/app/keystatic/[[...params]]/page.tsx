73ed3d5dcccb9ddbb6d4d6df45864c5c6e3606ca
Shun Kakinoki <shunkakinoki@gmail.com>
2024-09-29 00:20:45 +0900

    fix: app dir

--- apps/blog/app/keystatic/[[...params]]/page.tsx
+++ apps/blog/app/keystatic/[[...params]]/page.tsx
@@ -12,10 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export default function Page() {
-  // ---------------------------------------------------------------------------
-  // Render
-  // ---------------------------------------------------------------------------
-
-  return <></>;
-}
+export default function Page() {}
