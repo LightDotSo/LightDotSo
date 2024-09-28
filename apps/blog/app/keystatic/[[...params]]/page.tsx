c6c609502bd3e900806031f2d827f5bf66712b63
Shun Kakinoki <shunkakinoki@gmail.com>
2024-09-29 00:25:19 +0900

    chore: update

--- apps/blog/app/keystatic/[[...params]]/page.tsx
+++ apps/blog/app/keystatic/[[...params]]/page.tsx
@@ -12,4 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export default function Page() {}
+export default function Page() {
+  // ---------------------------------------------------------------------------
+  // Render
+  // ---------------------------------------------------------------------------
+
+  return <div />;
+}
