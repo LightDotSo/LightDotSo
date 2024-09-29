b875f0e065a9433c19315142afeb1f1922eedec7
Shun Kakinoki <shunkakinoki@gmail.com>
2024-09-30 06:44:48 +0900

    chore: update

--- packages/const/src/address.ts
+++ packages/const/src/address.ts
@@ -17,17 +17,14 @@ import type { Address } from "viem";
 export enum ContractAddress {
   ENTRYPOINT_V060_ADDRESS = "Entrypoint v0.6.0",
   ENTRYPOINT_V070_ADDRESS = "Entrypoint v0.7.0",
-  ENTRYPOINT_ADDRESS = "Entrypoint",
   CREATE2_DEPLOYER_ADDRESS = "Create2 Deployer Address",
   IMMUTABLE_CREATE2_FACTORY_ADDRESS = "Immutable Create2 Factory Address",
   LIGHT_WALLET_FACTORY_V010_ADDRESS = "Light Wallet Factory v0.1.0",
   LIGHT_WALLET_FACTORY_V020_ADDRESS = "Light Wallet Factory v0.2.0",
   LIGHT_WALLET_FACTORY_V030_ADDRESS = "Light Wallet Factory v0.3.0",
-  LIGHT_WALLET_FACTORY_ADDRESS = "Light Wallet Factory",
   LIGHT_WALLET_FACTORY_V010_IMPLEMENTATION = "Light Wallet Factory v0.1.0 Implementation",
   LIGHT_WALLET_FACTORY_V020_IMPLEMENTATION = "Light Wallet Factory v0.2.0 Implementation",
   LIGHT_WALLET_FACTORY_V030_IMPLEMENTATION = "Light Wallet Factory v0.3.0 Implementation",
-  LIGHT_WALLET_FACTORY_IMPLEMENTATION = "Light Wallet Factory Implementation",
 }
 
 export const CONTRACT_ADDRESSES: {
@@ -39,21 +36,15 @@ export const CONTRACT_ADDRESSES: {
     "0x00000000001269b052C004FFB71B47AB22C898B0",
   [ContractAddress.LIGHT_WALLET_FACTORY_V030_ADDRESS]:
     "0x000000000048C2e27c97B9978c9B27f6937A40db",
-  [ContractAddress.LIGHT_WALLET_FACTORY_ADDRESS]:
-    "0x000000000048C2e27c97B9978c9B27f6937A40db",
   [ContractAddress.LIGHT_WALLET_FACTORY_V010_IMPLEMENTATION]:
     "0x8FB3cFDf2082C2be7D3205D361067748Ea1aBF63",
   [ContractAddress.LIGHT_WALLET_FACTORY_V020_IMPLEMENTATION]:
     "0x040D53C5DDE1762F7cac48d5467E88236d4873d7",
   [ContractAddress.LIGHT_WALLET_FACTORY_V030_IMPLEMENTATION]:
     "0x5d02B6345898b193B66CA89E7A693CC11DC72954",
-  [ContractAddress.LIGHT_WALLET_FACTORY_IMPLEMENTATION]:
-    "0x5d02B6345898b193B66CA89E7A693CC11DC72954",
   [ContractAddress.ENTRYPOINT_V060_ADDRESS]:
     "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
   [ContractAddress.ENTRYPOINT_V070_ADDRESS]:
-    "0x0000000071727de22e5e9d8baf0edac6f37da032",
-  [ContractAddress.ENTRYPOINT_ADDRESS]:
     "0x0000000071727de22e5e9d8baf0edac6f37da032",
   [ContractAddress.CREATE2_DEPLOYER_ADDRESS]:
     "0x4e59b44847b379578588920cA78FbF26c0B4956C",
@@ -61,25 +52,24 @@ export const CONTRACT_ADDRESSES: {
     "0x0000000000ffe8b47b3e2130213b802212439497",
 };
 
-// biome-ignore lint/suspicious/noConsole: <explanation>
-console.assert(
-  CONTRACT_ADDRESSES[ContractAddress.ENTRYPOINT_ADDRESS] ===
+export enum LatestContractAddress {
+  ENTRYPOINT_ADDRESS = "Entrypoint",
+  LIGHT_WALLET_FACTORY_ADDRESS = "Light Wallet Factory",
+  LIGHT_WALLET_FACTORY_IMPLEMENTATION_ADDRESS = "Light Wallet Factory Implementation",
+}
+
+export const LATEST_CONTRACT_ADDRESSES: {
+  readonly [key in LatestContractAddress]: Address;
+} = {
+  [LatestContractAddress.ENTRYPOINT_ADDRESS]:
     CONTRACT_ADDRESSES[ContractAddress.ENTRYPOINT_V070_ADDRESS],
-);
-
-// biome-ignore lint/suspicious/noConsole: <explanation>
-console.assert(
-  CONTRACT_ADDRESSES[ContractAddress.LIGHT_WALLET_FACTORY_ADDRESS] ===
+  [LatestContractAddress.LIGHT_WALLET_FACTORY_ADDRESS]:
     CONTRACT_ADDRESSES[ContractAddress.LIGHT_WALLET_FACTORY_V030_ADDRESS],
-);
-
-// biome-ignore lint/suspicious/noConsole: <explanation>
-console.assert(
-  CONTRACT_ADDRESSES[ContractAddress.LIGHT_WALLET_FACTORY_IMPLEMENTATION] ===
+  [LatestContractAddress.LIGHT_WALLET_FACTORY_IMPLEMENTATION_ADDRESS]:
     CONTRACT_ADDRESSES[
       ContractAddress.LIGHT_WALLET_FACTORY_V030_IMPLEMENTATION
     ],
-);
+};
 
 // @ts-expect-error
 export const WALLET_FACTORY_ENTRYPOINT_MAPPING: {
