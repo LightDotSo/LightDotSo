3430ed0a29d8b320438aedf0f6adef909e6a2910
Shun Kakinoki <shunkakinoki@gmail.com>
2024-09-29 09:58:38 +0900

    chore: update

--- apps/app/app/(demo)/demo/overview/@nav/page.tsx
+++ apps/app/app/(demo)/demo/overview/@nav/page.tsx
@@ -12,16 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// import OriginalPage from "@/app/(wallet)/[address]/overview/@nav/page";
+import OriginalPage from "@/app/(wallet)/[address]/overview/@nav/page";
 
 // -----------------------------------------------------------------------------
-// Page
+// Original Page
 // -----------------------------------------------------------------------------
 
-export default function Page() {
-  // ---------------------------------------------------------------------------
-  // Render
-  // ---------------------------------------------------------------------------
-
-  return null;
-}
+export default OriginalPage;
