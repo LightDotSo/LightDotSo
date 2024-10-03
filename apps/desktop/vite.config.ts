2426e7fd3f0e7038897490b52ec8c2329e7dabe4
Shun Kakinoki <shunkakinoki@gmail.com>
2024-10-04 04:41:09 +0900

    chore: update

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,8 +1,22 @@
+// Copyright 2023-2024 LightDotSo.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig(() => ({
   plugins: [react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
@@ -20,7 +34,7 @@ export default defineConfig(async () => ({
     // Tauri supports es2021
     target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
     // don't minify for debug builds
-    minify: process.env.TAURI_DEBUG ? false : "esbuild",
+    // minify: process.env.TAURI_DEBUG ? false : "esbuild",
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
   },
