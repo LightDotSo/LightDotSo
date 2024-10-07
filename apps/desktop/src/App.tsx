79e0e0011c4df34ee3ca9e0ea0c62b99d3fb901e
Shun Kakinoki <shunkakinoki@gmail.com>
2024-10-08 08:52:42 +0900

    fix: app desktop

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { invoke } from "@tauri-apps/api";
+import { invoke } from "@tauri-apps/api/core";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
