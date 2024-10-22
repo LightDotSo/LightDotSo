31c5f7bd8f16474ef7c18039319d1a604da6e0f7
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2024-10-22 22:50:59 +0900

    chore: update (#2582)

--- packages/nuqs/src/index.ts
+++ packages/nuqs/src/index.ts
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+export { NuqsAdapter } from "nuqs/adapters/next/app";
+
 export {
   abiEncodedParser,
   useAbiEncodedQueryState,
